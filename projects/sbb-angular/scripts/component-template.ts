1a448b7cf4a32d1e08bc2b1ef0faaa8f8769603c
DAresta <davide.aresta@finconsgroup.com>
2018-09-11 14:20:14 +0200

    [SACL-22] fixes line feed at the end of file without using comment for ts:lint

--- projects/sbb-angular/scripts/component-template.ts
+++ projects/sbb-angular/scripts/component-template.ts
@@ -1,16 +1,13 @@
-
-
 exports.getTemplate = function (iconSelector, iconTemplate, iconComponentName) {
   return `import { Component } from '@angular/core';
-        @Component({
-          selector: '${iconSelector}',
-          // tslint:disable-next-line:max-line-length
-          template: '${iconTemplate}',
-          styles: []
-        })
-        export class ${iconComponentName} {
-          constructor() { }
-          // tslint:disable-next-line:eofline
-        }`;
+@Component({
+  selector: '${iconSelector}',
+  // tslint:disable-next-line:max-line-length
+  template: '${iconTemplate}',
+  styles: []
+})
+export class ${iconComponentName} {
+  constructor() { }
+}\n`;
 };
 
