a29ff5b3691384b6680f605a4678fae0b6e531b1
DAresta <davide.aresta@finconsgroup.com>
2018-09-26 17:50:02 +0200

    [SACL-23] fixes coding style

--- src/app/services/icon-ui.service.ts
+++ src/app/services/icon-ui.service.ts
@@ -8,47 +8,60 @@ import { UiComponent } from '../shared/ui-component';
 })
 export class IconUiService {
 
-  getUiIconByRouterLink(name: any) : UiIcon {
-    return SBBComponentsMapping.icons.filter(uiIcon => uiIcon.name.toLowerCase().indexOf(name.toLowerCase()) > -1)[0];
+  getUiIconByRouterLink(name: any): UiIcon {
+    return SBBComponentsMapping.icons
+      .find((uiIcon) => uiIcon.name.localeCompare(name, 'en', { sensitivity: 'base' }) === 0);
   }
 
-  getUiComponentByRouterLink(name: any) : UiComponent {
-    let foundUiIcons : UiIcon[] = [];
-    foundUiIcons = SBBComponentsMapping.icons.filter(uiIcon => uiIcon.name.toLowerCase().indexOf(name.toLowerCase()) > -1);
-    // tslint:disable-next-line
-    return new UiComponent(foundUiIcons[0]['name'],foundUiIcons[0]['name'],foundUiIcons[0]['name'],'Subtitel goes here ...',true,false,['Davide Aresta', 'Stefan Meili'],'Description goes here ...',foundUiIcons[0]['selector'],'Import text.','Getting started text.','Model binding text.','Icon text.','Auto resize text.','Properties text.','Events text.','Styling text.','Dependencies text.');
+  getUiComponentByRouterLink(name: any): UiComponent {
+    const foundUiIcon: UiIcon = SBBComponentsMapping.icons
+      .find(uiIcon => uiIcon.name.localeCompare(name, 'en', { sensitivity: 'base' }) === 0);
+    return new UiComponent(foundUiIcon.name,
+      foundUiIcon.name,
+      foundUiIcon.name,
+      'Subtitel goes here ...',
+      true,
+      false,
+      ['Davide Aresta', 'Stefan Meili'],
+      'Description goes here ...',
+      foundUiIcon.selector,
+      'Import text.',
+      'Getting started text.',
+      'Model binding text.',
+      'Icon text.',
+      'Auto resize text.',
+      'Properties text.',
+      'Events text.',
+      'Styling text.',
+      'Dependencies text.');
   }
 
-  getUiIconsBySearchValue(searchValue : any) {
+  getUiIconsBySearchValue(searchValue: any) {
 
-    let foundUiIcons : UiIcon[] = [];
-    if(searchValue.length > 0) {
-       foundUiIcons = SBBComponentsMapping.icons.filter(
-         uiIcon => uiIcon.name.toLowerCase().indexOf(searchValue.toLowerCase()) > -1 ||
-                   uiIcon.selector.toLowerCase().indexOf(searchValue.toLowerCase()) > -1 ||
-                   uiIcon.tags.find(tag => tag.toLowerCase().indexOf(searchValue.toLowerCase()) > -1)
-       );
+    if (searchValue.length > 0) {
+      return SBBComponentsMapping.icons.filter(uiIcon =>
+        (new RegExp(searchValue, 'ig').test(uiIcon.name)) ||
+        (new RegExp(searchValue, 'ig').test(uiIcon.selector)) ||
+        uiIcon.tags.find(tag => (new RegExp(searchValue, 'ig').test(tag))))
+        .map(item => new UiIcon(item.name, item.selector, item.tags))
+        .map(uiIcon => {
+          if (new RegExp(searchValue, 'ig').test(uiIcon.name)) {
+            const index = uiIcon.name.toLowerCase().indexOf(searchValue.toLowerCase());
+            const preFix = uiIcon.name.substring(0, index);
+            const sufFix = uiIcon.name.substring(index + searchValue.length);
+            const searchText = uiIcon.name.substring(index, index + searchValue.length);
+            const newName = `${preFix}<b>${searchText}</b>${sufFix}`;
+            uiIcon.name = newName;
+          }
+          return uiIcon;
+        })
+        .sort((a, b) => a.name.localeCompare(b.name));
     } else {
-      return foundUiIcons;
+      return [];
     }
-
-    const newFoundUIIcons : UiIcon[] = foundUiIcons.map(item => new UiIcon(item.name, item.selector, item.tags));
-
-    for(const uiIcon of newFoundUIIcons) {
-        if(uiIcon.name.toLowerCase().indexOf(searchValue.toLowerCase()) > -1) {
-           const index = uiIcon.name.toLowerCase().indexOf(searchValue.toLowerCase());
-           const preFix = uiIcon.name.substring(0, index);
-           const sufFix = uiIcon.name.substring(index + searchValue.length);
-           const searchText = uiIcon.name.substring(index, index + searchValue.length);
-           const newName = preFix + '<b>' + searchText + '</b>' + sufFix;
-           uiIcon.name = newName;
-        }
-    }
-
-    return newFoundUIIcons.sort((a,b) => a.name.localeCompare(b.name));
   }
 
-  getAll() : Array<UiIcon> {
-    return  SBBComponentsMapping.icons.sort((a,b) => a.name.localeCompare(b.name));
+  getAll(): Array<UiIcon> {
+    return SBBComponentsMapping.icons.sort((a, b) => a.name.localeCompare(b.name));
   }
 }
