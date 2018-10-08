114a346a0c1704d5b7e0a840646896cc5cb6e6c9
DAresta <davide.aresta@finconsgroup.com>
2018-10-08 12:58:31 +0200

    [SACL-40] imports cdk module in textarea spec test file

--- projects/sbb-angular/src/lib/textarea/textarea/textarea.component.spec.ts
+++ projects/sbb-angular/src/lib/textarea/textarea/textarea.component.spec.ts
@@ -1,6 +1,7 @@
 import { async, ComponentFixture, TestBed } from '@angular/core/testing';
 
 import { TextareaComponent } from './textarea.component';
+import { TextFieldModule } from '@angular/cdk/text-field';
 
 describe('TextAreaBasicComponent', () => {
   let component: TextareaComponent;
@@ -8,9 +9,10 @@ describe('TextAreaBasicComponent', () => {
 
   beforeEach(async(() => {
     TestBed.configureTestingModule({
-      declarations: [ TextareaComponent ]
+      imports: [TextFieldModule],
+      declarations: [TextareaComponent]
     })
-    .compileComponents();
+      .compileComponents();
   }));
 
   beforeEach(() => {
