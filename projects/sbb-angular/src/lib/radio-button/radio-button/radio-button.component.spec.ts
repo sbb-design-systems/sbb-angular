36bf5374d5e2afdd906a3b4054ca9256a410b03b
Dario D'Oronzo <dario.doronzo@finconsgroup.com>
2018-10-09 14:13:31 +0200

    [SACL-12] edit component

--- projects/sbb-angular/src/lib/radio-button/radio-button/radio-button.component.spec.ts
+++ projects/sbb-angular/src/lib/radio-button/radio-button/radio-button.component.spec.ts
@@ -1,14 +1,33 @@
 import { async, ComponentFixture, TestBed } from '@angular/core/testing';
 
 import { RadioButtonComponent } from './radio-button.component';
+import { ChangeDetectionStrategy, Component } from '@angular/core';
+import { FormsModule } from '@angular/forms';
+import { CommonModule } from '@angular/common';
 
-describe('RadioButtonComponent', () => {
+@Component({
+  selector: 'sbb-radio-button-test',
+  template: '<sbb-radio-button [(ngModel)]="testValue" id="test-radio-1" name="test-radio"></sbb-radio-button>' +
+            '<label for="test-radio-1">Test radio button value</label><br />' +
+            '{{testValue}}'
+})
+class RadioButtonTestComponent {
+  testValue = true;
+}
+
+fdescribe('RadioButtonComponent', () => {
   let component: RadioButtonComponent;
+  let mockComponent: RadioButtonTestComponent;
   let fixture: ComponentFixture<RadioButtonComponent>;
+  let mockComponentFixture: ComponentFixture<RadioButtonTestComponent>;
 
   beforeEach(async(() => {
     TestBed.configureTestingModule({
-      declarations: [ RadioButtonComponent ]
+      imports: [CommonModule, FormsModule],
+      declarations: [ RadioButtonComponent, RadioButtonTestComponent ]
+    })
+    .overrideComponent(RadioButtonComponent, {
+      set: { changeDetection: ChangeDetectionStrategy.Default }
     })
     .compileComponents();
   }));
@@ -16,10 +35,19 @@ describe('RadioButtonComponent', () => {
   beforeEach(() => {
     fixture = TestBed.createComponent(RadioButtonComponent);
     component = fixture.componentInstance;
+
+    mockComponentFixture = TestBed.createComponent(RadioButtonTestComponent);
+    mockComponent = mockComponentFixture.componentInstance;
+
     fixture.detectChanges();
+    mockComponentFixture.detectChanges();
   });
 
-  it('should create', () => {
-    expect(component).toBeTruthy();
+  // it('should create', () => {
+  //   expect(component).toBeTruthy();
+  // });
+
+  it('should create mock component', () => {
+    expect(mockComponent).toBeTruthy();
   });
 });
--- projects/sbb-angular/src/lib/radio-button/radio-button/radio-button.component.ts
+++ projects/sbb-angular/src/lib/radio-button/radio-button/radio-button.component.ts
@@ -1,22 +1,46 @@
-import { Component, OnInit, HostBinding } from '@angular/core';
+import { Component, OnInit, Input, ChangeDetectionStrategy, forwardRef } from '@angular/core';
+import { ControlValueAccessor, NG_VALUE_ACCESSOR } from '@angular/forms';
 
 @Component({
   selector: 'sbb-radio-button',
   templateUrl: './radio-button.component.html',
-  styleUrls: ['./radio-button.component.css']
+  styleUrls: ['./radio-button.component.css'],
+  providers: [{
+    provide: NG_VALUE_ACCESSOR,
+    useExisting: forwardRef(() => RadioButtonComponent),
+    multi: true,
+  }],
+  changeDetection: ChangeDetectionStrategy.OnPush
 })
-export class RadioButtonComponent implements OnInit {
+export class RadioButtonComponent implements OnInit, ControlValueAccessor {
 
-  @HostBinding('id') id: string;
-  @HostBinding('value') value: string;
-  @HostBinding('name') name: string;
-  @HostBinding('disabled') disabled: boolean;
-  @HostBinding('checked') checked: boolean;
-  @HostBinding('unchecked') unchecked: boolean;
+  @Input() id: string;
+  @Input() name: string;
+  @Input() value: string;
+  @Input() checked: boolean;
 
-  constructor() { }
 
-  ngOnInit() {
+  ngOnInit(): void {
+    console.log('ngOnInit: Method not implemented.');
+  }
+
+  writeValue(newValue: any): void {
+    console.log('writeValue: Method not implemented.');
+    if(newValue) {
+      this.value = newValue;
+    }
+  }
+
+  registerOnChange(fn: any): void {
+    console.log('registerOnChange: Method not implemented.');
+  }
+
+  registerOnTouched(fn: any): void {
+    console.log('registerOnTouched: Method not implemented.');
+  }
+
+  setDisabledState?(isDisabled: boolean): void {
+    console.log('setDisabledState: Method not implemented.');
   }
 
 }
