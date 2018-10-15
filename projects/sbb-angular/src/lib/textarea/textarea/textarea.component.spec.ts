9dad1abffe3d90f3cdee996cbf346c8d9b12c96c
DAresta <davide.aresta@finconsgroup.com>
2018-10-15 09:42:58 +0200

    [SACL-41] creates tests for digits counter

--- projects/sbb-angular/src/lib/textarea/textarea/textarea.component.spec.ts
+++ projects/sbb-angular/src/lib/textarea/textarea/textarea.component.spec.ts
@@ -2,8 +2,10 @@ import { async, ComponentFixture, TestBed } from '@angular/core/testing';
 
 import { TextareaComponent } from './textarea.component';
 import { TextFieldModule } from '@angular/cdk/text-field';
+import { ChangeDetectionStrategy } from '@angular/core';
+import { By } from '@angular/platform-browser';
 
-describe('TextAreaBasicComponent', () => {
+describe('TextareaComponent', () => {
   let component: TextareaComponent;
   let fixture: ComponentFixture<TextareaComponent>;
 
@@ -11,8 +13,14 @@ describe('TextAreaBasicComponent', () => {
     TestBed.configureTestingModule({
       imports: [TextFieldModule],
       declarations: [TextareaComponent]
+    }).overrideComponent(TextareaComponent, {
+      set: { changeDetection: ChangeDetectionStrategy.Default }
     })
       .compileComponents();
+    fixture = TestBed.createComponent(TextareaComponent);
+    component = fixture.componentInstance;
+    fixture.detectChanges();
+
   }));
 
   beforeEach(() => {
@@ -23,5 +31,63 @@ describe('TextAreaBasicComponent', () => {
 
   it('should create', () => {
     expect(component).toBeTruthy();
+  });
+
+  it('should be required', () => {
+    component.required = true;
+    fixture.detectChanges();
+    const counterDiv = fixture.debugElement.query(By.css('.ng-invalid'));
+  });
+
+});
+
+
+fdescribe('TextareaComponent digits counter', () => {
+  let component: TextareaComponent;
+  let fixture: ComponentFixture<TextareaComponent>;
+
+  beforeEach(async(() => {
+    TestBed.configureTestingModule({
+      imports: [TextFieldModule],
+      declarations: [TextareaComponent]
+    }).overrideComponent(TextareaComponent, {
+      set: { changeDetection: ChangeDetectionStrategy.Default }
+    })
+      .compileComponents();
+
+  }));
+
+  beforeEach(() => {
+    fixture = TestBed.createComponent(TextareaComponent);
+    component = fixture.componentInstance;
+    component.maxlength = 20;
+    component.textContent = 'SBB';
+    fixture.detectChanges();
+  });
+
+  it('should have a digits counter on the bottom right', () => {
+    component.maxlength = 20;
+    component.textContent = 'SBB';
+    fixture.detectChanges();
+    const counterDiv = fixture.debugElement.query(By.css('div'));
+    expect(counterDiv).toBeTruthy();
+  });
+
+  it('should have a 17 value', async () => {
+    component.maxlength = 20;
+    component.writeValue('SBB');
+    fixture.detectChanges();
+    component.writeValue(component.textContent + ' ');
+    fixture.detectChanges();
+    const counterDiv = fixture.debugElement.query(By.css('div'));
+    expect(counterDiv.nativeElement.textContent).toBe('Noch 16 Zeichen');
+
+  });
+
+  it('shoud disappear', () => {
+    component.maxlength = null;
+    fixture.detectChanges();
+    const counterDiv = fixture.debugElement.query(By.css('div'));
+    expect(counterDiv).toBeNull();
   });
 });
