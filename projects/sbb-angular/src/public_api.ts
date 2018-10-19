5a3113b8f0ad82108cc87bcf343aee0d3b966e0b
DAresta <davide.aresta@finconsgroup.com>
2018-10-19 09:48:02 +0200

    [SACL-7] adds index suffix in the public_api paths

--- projects/sbb-angular/src/public_api.ts
+++ projects/sbb-angular/src/public_api.ts
@@ -1,7 +1,7 @@
 /*
   * Public API Surface of sbb-angular
   */
-export * from './lib/svg-icons-components';
-export * from './lib/textarea';
-export * from './lib/links';
-export * from './lib/autocomplete';
+export * from './lib/svg-icons-components/index';
+export * from './lib/textarea/index';
+export * from './lib/links/index';
+export * from './lib/autocomplete/index';
