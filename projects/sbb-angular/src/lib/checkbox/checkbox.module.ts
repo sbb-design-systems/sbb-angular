b3db0597d07f935d55193230441d3b3476c5634b
Dario D'Oronzo <dario.doronzo@finconsgroup.com>
2018-10-23 17:58:11 +0200

    [SACL-13] aot bugfix

--- projects/sbb-angular/src/lib/checkbox/checkbox.module.ts
+++ projects/sbb-angular/src/lib/checkbox/checkbox.module.ts
@@ -1,7 +1,7 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 
-import { IconCommonModule } from '../svg-icons-components';
+import { IconCommonModule } from '../svg-icons-components/icon-common.module';
 import { CheckboxComponent } from './checkbox/checkbox.component';
 
 @NgModule({
