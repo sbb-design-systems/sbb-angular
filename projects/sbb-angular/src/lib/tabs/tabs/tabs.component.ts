0ebff2d810d6bb2e8cf68a60ec8fd8f690e65caa
DAresta <davide.aresta@finconsgroup.com>
2018-11-23 17:39:22 +0100

    merge from develop

---
+++ projects/sbb-angular/src/lib/tabs/tabs/tabs.component.ts
@@ -0,0 +1,174 @@
+import {
+  Component,
+  ContentChildren,
+  QueryList,
+  AfterContentInit,
+  ComponentFactoryResolver,
+  ElementRef,
+  ChangeDetectionStrategy,
+  OnDestroy,
+  ChangeDetectorRef,
+  ViewEncapsulation,
+  ViewChildren
+} from '@angular/core';
+import { LEFT_ARROW, RIGHT_ARROW, UP_ARROW, DOWN_ARROW, TAB } from '@angular/cdk/keycodes';
+import { Observable, merge, of, Subscription } from 'rxjs';
+import { map, switchMap } from 'rxjs/operators';
+
+import { TabComponent } from '../tab/tab.component';
+
+let counter = 0;
+
+@Component({
+  selector: 'sbb-tabs',
+  templateUrl: './tabs.component.html',
+  styleUrls: ['./tabs.component.scss'],
+  changeDetection: ChangeDetectionStrategy.OnPush,
+  encapsulation: ViewEncapsulation.None
+})
+export class TabsComponent implements AfterContentInit, OnDestroy {
+
+  nameOfTabList = `sbb-tabs-${counter++}`;
+
+  tabListIndex = 0;
+
+  private allowedKeyCodes = [
+    LEFT_ARROW,
+    RIGHT_ARROW,
+    UP_ARROW,
+    DOWN_ARROW,
+    TAB
+  ];
+
+  @ContentChildren(TabComponent) tabs: QueryList<TabComponent>;
+  tabs$: Observable<TabComponent[]>;
+
+  @ViewChildren('label') labels: QueryList<ElementRef>;
+
+  private _tabsSubscription = Subscription.EMPTY;
+
+  constructor(public componentFactoryResolver: ComponentFactoryResolver,
+    public elementRef: ElementRef,
+    private _changeDetector: ChangeDetectorRef) { }
+
+  ngAfterContentInit() {
+
+    this.checkNumberOfTabs();
+
+    this.initTabs();
+
+    this._tabsSubscription = this.tabs$
+      .pipe(
+        map(tabs => tabs.map(t => t._stateChanges)),
+        switchMap(stateChanges => merge(...stateChanges)))
+      .subscribe(() => this._changeDetector.markForCheck());
+
+  }
+
+  initTabs() {
+    this.tabs$ = merge<TabComponent[]>(of(this.tabs.toArray()), this.tabs.changes);
+
+    const activeTabs = this.tabs.filter(tab => tab.active);
+
+    if (activeTabs.length !== 1) {
+      this.selectTab(this.tabs.first);
+    }
+  }
+
+  ngOnDestroy() {
+    this._tabsSubscription.unsubscribe();
+  }
+
+  openFirstTab() {
+    this.tabListIndex = 0;
+    this.selectTab(this.tabs.first);
+  }
+
+  openTabByIndex(tabIndex: number) {
+    const tabToSelect = this.tabs.toArray()[tabIndex];
+    this.selectTab(tabToSelect);
+  }
+
+  selectTab(tab: TabComponent) {
+    this.tabs.forEach((t, index) => {
+      if (t.labelId === tab.labelId) {
+        this.tabListIndex = index;
+      }
+      t.active = false;
+      t.tabindex = -1;
+      t.tabMarkForCheck();
+    });
+
+    tab.active = true;
+    tab.tabindex = 0;
+    tab.tabMarkForCheck();
+  }
+
+  onKeyUp(event) {
+    // respond only to arrows and tab
+    if (this.allowedKeyCodes.indexOf(event.keyCode) !== -1) {
+      if (event.keyCode === TAB) {
+        this.tabListIndex = this.tabs.map(t => t.active).indexOf(true);
+      } else {
+        this.handleKeyUp(event.keyCode);
+      }
+    }
+  }
+
+  private handleKeyUp(keyCode) {
+    const tabLabels = this.labels.toArray();
+    const hasReachEnd = this.handleKeyCodeReturnHasReachEnd(keyCode);
+    const tabToFocus = tabLabels[this.tabListIndex];
+
+    if (tabToFocus.nativeElement.disabled && !hasReachEnd) {
+      // go to next
+      this.handleKeyUp(keyCode);
+    } else if (tabToFocus.nativeElement.disabled && hasReachEnd) {
+      // reached end and no focusable tabs found. reverse direction to find a focusable one
+      if (keyCode === LEFT_ARROW || keyCode === UP_ARROW) {
+        this.handleKeyUp(RIGHT_ARROW);
+      } else {
+        this.handleKeyUp(LEFT_ARROW);
+      }
+    } else {
+      tabLabels[this.tabListIndex].nativeElement.focus();
+    }
+
+  }
+
+  private handleKeyCodeReturnHasReachEnd(keyCode) {
+    let hasReachEnd = false;
+
+    switch (keyCode) {
+      case LEFT_ARROW:
+      case UP_ARROW:
+        if (this.tabListIndex > 0) {
+          this.tabListIndex -= 1;
+        } else {
+          hasReachEnd = true;
+        }
+        break;
+
+      case RIGHT_ARROW:
+      case DOWN_ARROW:
+        if (this.tabListIndex < this.tabs.length - 1) {
+          this.tabListIndex += 1;
+        } else {
+          hasReachEnd = true;
+        }
+        break;
+
+      default:
+        this.tabListIndex = this.tabListIndex;
+    }
+
+    return hasReachEnd;
+  }
+
+  private checkNumberOfTabs(): void {
+    if (this.tabs.length < 2) {
+      throw new Error(`The number of tabs must be at least 2`);
+    }
+  }
+
+}
