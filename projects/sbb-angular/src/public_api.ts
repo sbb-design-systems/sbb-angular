fbce6fa37cca27fe861b283065f9fabd5db327d6
DAresta <davide.aresta@finconsgroup.com>
2018-11-27 14:45:20 +0100

    [SACL-10] changes aria-label attrs to work with i18n

--- projects/sbb-angular/src/public_api.ts
+++ projects/sbb-angular/src/public_api.ts
@@ -10,6 +10,5 @@ export * from './lib/button/index';
 export * from './lib/radio-button/index';
 export * from './lib/checkbox/index';
 export * from './lib/tabs/index';
-export * from './lib/time-input/index';
 export * from './lib/datepicker/index';
-export * from './lib/tabs/index';
+export * from './lib/time-input/index';
