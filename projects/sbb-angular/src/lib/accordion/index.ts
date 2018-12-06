2e75f403f172a8a3fc495ca8b6e23fc4d9d9914e
FINCONSGROUP\marco.sut <marco.sut@finconsgroup.com>
2018-12-06 13:09:19 +0100

    [SACL-15] Empty to trigger build again

--- projects/sbb-angular/src/lib/accordion/index.ts
+++ projects/sbb-angular/src/lib/accordion/index.ts
@@ -5,3 +5,4 @@ export * from './expansion-panel/expansion-panel.component';
 export * from './expansion-panel-header/expansion-panel-header.component';
 export * from './expansion-panel/expansion-panel-content';
 export * from './accordion/accordion-animations';
+
