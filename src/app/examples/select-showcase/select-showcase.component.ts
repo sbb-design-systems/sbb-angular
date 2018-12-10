2a569a8b4f8942cf778392592c77b812e075cfe6
DAresta <davide.aresta@finconsgroup.com>
2018-12-10 15:30:46 +0100

    [SACL-9] improves showcase

--- src/app/examples/select-showcase/select-showcase.component.ts
+++ src/app/examples/select-showcase/select-showcase.component.ts
@@ -14,9 +14,26 @@ export interface Food {
 export class SelectShowcaseComponent {
 
   foods: Food[] = [
-    {value: 'steak-0', viewValue: 'Steak'},
-    {value: 'pizza-1', viewValue: 'Pizza'},
-    {value: 'tacos-2', viewValue: 'Tacos'}
+    { value: 'beefsteak-0', viewValue: 'Beefsteak' },
+    { value: 'pizza-1', viewValue: 'Pizza' },
+    { value: 'nudeln-2', viewValue: 'Nudeln' }
   ];
+
+  foodFromTheWorld = [{
+    nation: 'Italien',
+    food: [
+      { value: 'lasagna', viewValue: 'Lasagna' },
+      { value: 'parmigiana', viewValue: 'Parmigiana' },
+      { value: 'krm', viewValue: 'Kartoffeln, Reis und Muscheln' }
+    ]
+  }, {
+    nation: 'Deutschland',
+    food: [
+      { value: 'weißwurst', viewValue: 'Weißwurst' },
+      { value: 'eisbein', viewValue: 'Eisbein' },
+      { value: 'rheinische-muscheln', viewValue: 'Rheinische Muscheln' }
+    ]
+  }];
+
 
 }
