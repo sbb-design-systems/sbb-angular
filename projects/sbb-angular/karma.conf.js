16e417973a6848d239e2ec1a531212bd6bd6998c
DAresta <davide.aresta@finconsgroup.com>
2018-12-13 10:55:03 +0100

    [SACL-9] installs and configures puppeteer to use ChromeHeadless during jenkins test phase

--- projects/sbb-angular/karma.conf.js
+++ projects/sbb-angular/karma.conf.js
@@ -1,6 +1,8 @@
 // Karma configuration file, see link for more information
 // https://karma-runner.github.io/1.0/config/configuration-file.html
 
+process.env.CHROME_BIN = require('puppeteer').executablePath();
+
 module.exports = function (config) {
   config.set({
     basePath: '',
@@ -35,7 +37,7 @@ module.exports = function (config) {
     colors: true,
     logLevel: config.LOG_INFO,
     autoWatch: true,
-    browsers: ['Chrome'],
+    browsers: ['ChromeHeadless'],
     singleRun: false,
     browserDisconnectTolerance: 2,
     browserNoActivityTimeout: 50000,
