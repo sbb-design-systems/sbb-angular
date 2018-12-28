0d5a6f6222432ce9cf576a627dc483c44d21b9b8
Davide Aresta <davide.aresta@finconsgroup.com>
2018-12-28 15:29:04 +0100

    [SACL-19] initiazes processflow

--- projects/sbb-angular/src/lib/processflow/processflow/processflow.component.ts
+++ projects/sbb-angular/src/lib/processflow/processflow/processflow.component.ts
@@ -1,5 +1,12 @@
 import { Component, OnInit, ChangeDetectionStrategy } from '@angular/core';
 
+export interface ProcessflowState {
+  route: string;
+  title: string;
+  active: boolean;
+}
+
+
 @Component({
   selector: 'sbb-processflow',
   templateUrl: './processflow.component.html',
