8a8d5d48ec15b7c8b261069ed4ce3fe436c06ab6
FINCONSGROUP\marco.sut <marco.sut@finconsgroup.com>
2019-01-02 11:09:46 +0100

    [SACL-1] Lists

--- src/app/typography/typography.component.ts
+++ src/app/typography/typography.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, ViewChildren, QueryList, ElementRef } from '@angular/core';
 
 @Component({
   selector: 'sbb-typography',
@@ -6,6 +6,9 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./typography.component.scss']
 })
 export class TypographyComponent {
+
+  @ViewChildren('pageLink') pageLinks: QueryList<ElementRef>;
+
   editorOptions = {
     theme: 'vs-dark',
     codeLens: false,
@@ -30,4 +33,135 @@ export class TypographyComponent {
     bibendum cursus felis.
   </li>
 </ul>`;
+
+  codeNestedUnorderedList = `
+<ul>
+  <li>
+    Consectetur adipiscing elit. Mauris ac velit turpis. Integer vehicula sagittis libero, id pretium metus rutrum sed.
+    <ul>
+      <li>
+        Curabitur vitae vehicula sem. Praesent et convallis augue.
+          Quisque tristique eleifend augue, vitae consectetur ipsum porta nec. Nullam eget ex pretium, tincidunt felis ac, commodo arcu.
+        <ul>
+          <li>
+            Curabitur vitae vehicula sem. Praesent et convallis augue. Quisque tristique eleifend augue, vitae consectetur ipsum porta nec.
+          </li>
+        </ul>
+      </li>
+      <li>
+        Curabitur vitae vehicula sem. Praesent et convallis augue.
+          Quisque tristique eleifend augue, vitae consectetur ipsum porta nec. Nullam eget ex pretium, tincidunt felis ac, commodo arcu.
+      </li>
+      <li>
+        Curabitur vitae vehicula sem. Praesent et convallis augue.
+          Quisque tristique eleifend augue, vitae consectetur ipsum porta nec. Nullam eget ex pretium, tincidunt felis ac, commodo arcu.
+        <ul>
+          <li>
+            Curabitur vitae vehicula sem. Praesent et convallis augue. Quisque tristique eleifend augue, vitae consectetur ipsum porta nec.
+          </li>
+          <li>
+            Curabitur vitae vehicula sem. Praesent et convallis augue. Quisque tristique eleifend augue, vitae consectetur ipsum porta nec.
+          </li>
+          <li>
+            Curabitur vitae vehicula sem. Praesent et convallis augue. Quisque tristique eleifend augue, vitae consectetur ipsum porta nec.
+          </li>
+        </ul>
+      </li>
+      <li>
+        Curabitur vitae vehicula sem. Praesent et convallis augue.
+          Quisque tristique eleifend augue, vitae consectetur ipsum porta nec. Nullam eget ex pretium, tincidunt felis ac, commodo arcu.
+      </li>
+    </ul>
+  </li>
+  <li>
+    Sed non turpis sed metus lobortis blandit non id dui. Mauris ultrices dictum ornare. Cras sed condimentum nisl, bibendum cursus felis.
+  </li>
+</ul>`;
+
+  codeOrderedList = `
+<ol>
+  <li>
+    Consectetur adipiscing elit. Mauris ac velit turpis. Integer vehicula sagittis libero, id pretium metus rutrum sed.
+  </li>
+  <li>
+    Curabitur vitae vehicula sem. Praesent et convallis augue.
+      Quisque tristique eleifend augue, vitae consectetur ipsum porta nec. Nullam eget ex pretium, tincidunt felis ac, commodo arcu.
+  </li>
+  <li>
+    Sed non turpis sed metus lobortis blandit non id dui. Mauris ultrices dictum ornare. Cras sed condimentum nisl, bibendum cursus felis.
+  </li>
+</ol>`;
+
+  codeNestedOrderedList = `
+<ol>
+  <li>
+    Consectetur adipiscing elit. Mauris ac velit turpis. Integer vehicula sagittis libero, id pretium metus rutrum sed.
+    <ol>
+      <li>
+        Curabitur vitae vehicula sem. Praesent et convallis augue.
+         Quisque tristique eleifend augue, vitae consectetur ipsum porta nec. Nullam eget ex pretium, tincidunt felis ac, commodo arcu.
+        <ol>
+          <li>
+            Curabitur vitae vehicula sem. Praesent et convallis augue. Quisque tristique eleifend augue, vitae consectetur ipsum porta nec.
+          </li>
+        </ol>
+      </li>
+      <li>
+        Curabitur vitae vehicula sem. Praesent et convallis augue.
+         Quisque tristique eleifend augue, vitae consectetur ipsum porta nec. Nullam eget ex pretium, tincidunt felis ac, commodo arcu.
+      </li>
+    </ol>
+  </li>
+  <li>
+    Sed non turpis sed metus lobortis blandit non id dui. Mauris ultrices dictum ornare. Cras sed condimentum nisl, bibendum cursus felis.
+  </li>
+</ol>`;
+
+  codeMixedList = `
+<ol>
+  <li>Die Person mit einer Behinderung besitzt eine Begleiterkarteund einen gültigen Fahrausweis.
+   Als Fahrausweis gilt (dann reist die Begleitperson gratis):
+    <ul>
+      <li>Curabitur vitae vehicula sem. Praesent et convallis augue.
+       Quisque tristique eleifend augue, vitae consectetur ipsum porta nec. Nullam eget ex pretium, tincidunt felis ac, commodo arcu.</li>
+      <li>Curabitur vitae vehicula sem. Praesent et convallis augue.
+       Quisque tristique eleifend augue, vitae consectetur ipsum porta nec. Nullam eget ex pretium, tincidunt felis ac, commodo arcu.</li>
+    </ul>
+  </li>
+  <li>Die Begleitperson besitzt einen gültigen Fahrausweis.
+   Als Fahrausweis gilt (dann reist die Person mit einer Behinderung und einer Begleiterkarte gratis):
+    <ul>
+      <li>Curabitur vitae vehicula sem. Praesent et convallis augue.
+       Quisque tristique eleifend augue, vitae consectetur ipsum porta nec. Nullam eget ex pretium, tincidunt felis ac, commodo arcu.</li>
+      <li>Curabitur vitae vehicula sem. Praesent et convallis augue.
+       Quisque tristique eleifend augue, vitae consectetur ipsum porta nec. Nullam eget ex pretium, tincidunt felis ac, commodo arcu.</li>
+    </ul>
+  </li>
+</ol>
+<ul>
+  <li>Die Person mit einer Behinderung besitzt eine Begleiterkarteund einen gültigen Fahrausweis.
+   Als Fahrausweis gilt (dann reist die Begleitperson gratis):
+    <ol>
+      <li>Curabitur vitae vehicula sem. Praesent et convallis augue.
+       Quisque tristique eleifend augue, vitae consectetur ipsum porta nec. Nullam eget ex pretium, tincidunt felis ac, commodo arcu.</li>
+      <li>Curabitur vitae vehicula sem. Praesent et convallis augue.
+       Quisque tristique eleifend augue, vitae consectetur ipsum porta nec. Nullam eget ex pretium, tincidunt felis ac, commodo arcu.</li>
+    </ol>
+  </li>
+  <li>Die Begleitperson besitzt einen gültigen Fahrausweis.
+   Als Fahrausweis gilt (dann reist die Person mit einer Behinderung und einer Begleiterkarte gratis):
+    <ol>
+      <li>Curabitur vitae vehicula sem. Praesent et convallis augue.
+       Quisque tristique eleifend augue, vitae consectetur ipsum porta nec. Nullam eget ex pretium, tincidunt felis ac, commodo arcu.</li>
+      <li>Curabitur vitae vehicula sem. Praesent et convallis augue.
+       Quisque tristique eleifend augue, vitae consectetur ipsum porta nec. Nullam eget ex pretium, tincidunt felis ac, commodo arcu.</li>
+    </ol>
+  </li>
+</ul>`;
+
+
+  goToInpageLink(evt: any, pageLinkIndex: number) {
+    evt.preventDefault();
+    this.pageLinks.toArray()[pageLinkIndex].nativeElement.focus();
+  }
 }
