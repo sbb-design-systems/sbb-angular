2dab2f1770799cf8711423d9cb7c696c9e133870
DAresta <davide.aresta@finconsgroup.com>
2019-01-07 16:32:14 +0100

    [SACL-19] implements processflow

--- projects/sbb-angular/src/lib/processflow/processflow/processflow.component.ts
+++ projects/sbb-angular/src/lib/processflow/processflow/processflow.component.ts
@@ -22,16 +22,52 @@ export class ProcessflowComponent {
   steps: ProcessflowStep[] = [];
 
   @Output()
-  stepChange: EventEmitter<ProcessflowStep>;
+  stepChange: EventEmitter<ProcessflowStep> = new EventEmitter<ProcessflowStep>();
 
   constructor(private changeDetectorRef: ChangeDetectorRef) { }
 
-  changeStep(step: ProcessflowStep) {
-    this.steps.forEach((s) => {
+  nextStep() {
+    const activeStepIndex = this.findActiveStep(this.steps);
+    let activatedStep = false;
+    if (activeStepIndex < this.steps.length - 1) {
+      this.steps.forEach((s, i) => {
+        if (i > activeStepIndex && !activatedStep) {
+          s.active = true;
+          s.disabled = false;
+          activatedStep = true;
+        } else {
+          s.active = false;
+        }
+      });
+      this.changeDetectorRef.markForCheck();
+      this.stepChange.emit(this.steps[activeStepIndex + 1]);
+    }
+  }
+
+  private findActiveStep(steps: ProcessflowStep[]) {
+    return steps.findIndex(s => !!s.active);
+  }
+
+  stepClick($event: any, stepIndex: number) {
+    $event.preventDefault();
+    this.changeStep(stepIndex);
+  }
+
+  changeStep(index: number) {
+    const step = this.steps[index];
+    this.steps.forEach((s, i) => {
       s.active = false;
     });
     step.active = true;
     this.changeDetectorRef.markForCheck();
     this.stepChange.emit(step);
   }
+
+  disableStep(index: number) {
+    const step = this.steps[index];
+    step.disabled = true;
+    this.changeDetectorRef.markForCheck();
+
+  }
+
 }
--- src/app/examples/processflow-showcase/processflow-showcase.component.ts
+++ src/app/examples/processflow-showcase/processflow-showcase.component.ts
@@ -1,15 +1,31 @@
-import { Component, OnInit } from '@angular/core';
-import { ProcessflowStep } from 'projects/sbb-angular/src/public_api';
+import { Component, ViewChild, AfterViewInit } from '@angular/core';
+import { ProcessflowStep, ProcessflowComponent } from 'projects/sbb-angular/src/public_api';
 
 @Component({
   selector: 'sbb-processflow-showcase',
   templateUrl: './processflow-showcase.component.html',
   styleUrls: ['./processflow-showcase.component.scss']
 })
-export class ProcessflowShowcaseComponent {
+export class ProcessflowShowcaseComponent implements AfterViewInit {
+
+  logs: string[] = [];
+  logText: string;
 
   steps: ProcessflowStep[] = [
-    { title: '1. Intro', active: true, disabled: false },
-    { title: '2. Chapter 1', active: false, disabled: true }
+    { title: '1. Verbindung', active: true, disabled: false },
+    { title: '2. Reisedaten', active: false, disabled: true },
+    { title: '3. Zahlung', active: false, disabled: true },
+    { title: '4. Bestätigung', active: false, disabled: true }
+
   ];
+
+  @ViewChild('processflow') processflow: ProcessflowComponent;
+
+  ngAfterViewInit(): void {
+    this.processflow.stepChange.subscribe((s: ProcessflowStep) => {
+      this.logs.push('Step changed to ' + s.title);
+      this.logText = this.logs.reverse().join('\n');
+    });
+  }
+
 }
