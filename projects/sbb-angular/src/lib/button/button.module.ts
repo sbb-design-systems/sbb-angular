220a84f62c5367b5f24ffaa410d7b7588aafe3c5
Lukas Spirig <lukas.spirig@sbb.ch>
2019-02-08 14:23:27 +0100

    Replaced IconCommonModule with specific icons

--- projects/sbb-angular/src/lib/button/button.module.ts
+++ projects/sbb-angular/src/lib/button/button.module.ts
@@ -2,13 +2,11 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 
 import { ButtonComponent } from './button/button.component';
-import { IconCommonModule } from '../svg-icons-components/icon-common.module';
 import { ButtonIconDirective } from './button/button-icon.directive';
 
 @NgModule({
   imports: [
     CommonModule,
-    IconCommonModule
   ],
   declarations: [
     ButtonComponent,
--- projects/sbb-angular/src/lib/links/links.module.ts
+++ projects/sbb-angular/src/lib/links/links.module.ts
@@ -1,15 +1,34 @@
-import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { NgModule } from '@angular/core';
 
-import { IconCommonModule } from '../svg-icons-components/icon-common.module';
-
+import {
+  IconArrowDownModule,
+  IconDownloadModule,
+  IconFacebookModule,
+  IconGoogleplusModule,
+  IconInstagramModule,
+  IconLinkedinModule,
+  IconPinterestModule,
+  IconTwitterModule,
+  IconXingModule,
+  IconYoutubeModule,
+} from '../svg-icons/svg-icons';
 import { LinkComponent } from './link/link.component';
 import { SocialLinkComponent } from './social-link/social-link.component';
 
 @NgModule({
   imports: [
     CommonModule,
-    IconCommonModule
+    IconArrowDownModule,
+    IconDownloadModule,
+    IconFacebookModule,
+    IconGoogleplusModule,
+    IconInstagramModule,
+    IconPinterestModule,
+    IconTwitterModule,
+    IconYoutubeModule,
+    IconXingModule,
+    IconLinkedinModule,
   ],
   declarations: [
     LinkComponent,
