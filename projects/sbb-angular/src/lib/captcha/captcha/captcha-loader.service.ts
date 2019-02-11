38692de58f1c540ff3a171d066ffcccf3843857a
FINCONSGROUP\marco.sut <marco.sut@finconsgroup.com>
2019-02-11 17:28:34 +0100

    [SACL-100] Captcha fine tuning

--- projects/sbb-angular/src/lib/captcha/captcha/captcha-loader.service.ts
+++ projects/sbb-angular/src/lib/captcha/captcha/captcha-loader.service.ts
@@ -4,6 +4,7 @@ import {
   InjectionToken,
   Optional
 } from '@angular/core';
+import { DOCUMENT } from '@angular/common';
 import { BehaviorSubject, Observable } from 'rxjs';
 
 import { WindowRef } from './windowref.service';
@@ -30,8 +31,11 @@ export class CaptchaLoaderService {
   /** @internal */
   private _nonce: string;
 
+  private _document: Document;
+
   constructor(
     private _windowRef: WindowRef,
+    @Inject(DOCUMENT) document: any,
     @Optional() @Inject(RECAPTCHA_LANGUAGE) language?: string,
     @Optional() @Inject(RECAPTCHA_BASE_URL) baseUrl?: string,
     @Optional() @Inject(RECAPTCHA_NONCE) nonce?: string
@@ -39,6 +43,7 @@ export class CaptchaLoaderService {
     this._language = language;
     this._baseUrl = baseUrl;
     this._nonce = nonce;
+    this._document = document;
     this.init();
     this.ready = CaptchaLoaderService.ready.asObservable();
   }
@@ -52,7 +57,7 @@ export class CaptchaLoaderService {
       CaptchaLoaderService.ready.next(grecaptcha);
     };
     CaptchaLoaderService.ready = new BehaviorSubject<ReCaptchaV2.ReCaptcha>(null);
-    const script = document.createElement('script') as HTMLScriptElement;
+    const script = this._document.createElement('script') as HTMLScriptElement;
     script.innerHTML = '';
     const langParam = this._language ? '&hl=' + this._language : '';
     const baseUrl = this._baseUrl || RECAPTCHA_DEFAULT_BASE_URL;
@@ -62,6 +67,6 @@ export class CaptchaLoaderService {
     }
     script.async = true;
     script.defer = true;
-    document.head.appendChild(script);
+    this._document.head.appendChild(script);
   }
 }
