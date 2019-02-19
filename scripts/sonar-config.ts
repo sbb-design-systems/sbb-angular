4b05a7db3ae0c39471fb7599deaf10dde73529cf
Lukas Spirig <lukas.spirig@sbb.ch>
2019-02-19 14:43:25 +0100

    Fixed missing sonar config version and branch

--- scripts/sonar-config.ts
+++ scripts/sonar-config.ts
@@ -3,7 +3,9 @@ import { resolve } from 'path';
 
 const sonarConfigPath = resolve(__dirname, '../sonar-project.properties');
 const version = require('../package.json').version;
+const branch = process.env.BRANCH_NAME || 'develop';
+console.log(`Sonar Config: Set version (${version}) and branch (${branch})`);
 const sonarConfig = readFileSync(sonarConfigPath, 'utf8')
-    .replace(/sonar\.projectVersion=[^\r\n]/g, `sonar.projectVersion=${version}`)
-    .replace(/sonar\.branch=[^\r\n]/g, `sonar.branch=${process.env.BRANCH_NAME || 'develop'}`);
+    .replace(/sonar.projectVersion=[^\r\n]*/g, `sonar.projectVersion=${version}`)
+    .replace(/sonar.branch=[^\r\n]*/g, `sonar.branch=${branch}`);
 writeFileSync(sonarConfigPath, sonarConfig, 'utf8');
