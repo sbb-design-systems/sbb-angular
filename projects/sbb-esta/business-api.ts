e0e71d472b687a590c2f4e19d30ef5562e18fa1a
Lukas Spirig <lukas.spirig@sbb.ch>
2019-06-12 12:51:45 +0200

    Changed globals to common and extracted colors to own file

--- projects/sbb-esta/business-api.ts
+++ projects/sbb-esta/business-api.ts
@@ -4,3 +4,4 @@
  * DO NOT REMOVE THIS FILE, AS THIS IS REQUIRED TO IMPORT MODULES FROM angular-public IN angular-business.
  */
 export * from './angular-business/src/public-api';
+export * from './angular-public/src/lib/checkbox/checkbox';
