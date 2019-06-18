a96553755ce4a24ffb045fe3a91f202f55b41e92
E515964 <E515964@sbb.ch>
2019-06-18 13:43:30 +0200

    feat(business/button): expose button component for business applications

--- projects/sbb-esta/angular-public/src/lib/button/button/button.component.ts
+++ projects/sbb-esta/angular-public/src/lib/button/button/button.component.ts
@@ -1,14 +1,12 @@
 import {
   ChangeDetectionStrategy,
   Component,
-  ContentChild,
   HostBinding,
   Input,
-  TemplateRef,
   ViewEncapsulation
 } from '@angular/core';
 
-import { ButtonIconDirective } from './button-icon.directive';
+import { BaseButton } from './base-button';
 
 @Component({
   // tslint:disable-next-line:component-selector
@@ -18,54 +16,11 @@ import { ButtonIconDirective } from './button-icon.directive';
   changeDetection: ChangeDetectionStrategy.OnPush,
   encapsulation: ViewEncapsulation.None
 })
-export class ButtonComponent {
+export class ButtonComponent extends BaseButton {
   /**
    * Button modes available for different purposes.
    */
   @Input() mode: 'primary' | 'secondary' | 'ghost' | 'frameless' = 'primary';
-
-  /**
-   * Template that will contain icons.
-   * Use the *sbbButtonIcon structural directive to provide the desired icon.
-   */
-  @Input()
-  get icon(): TemplateRef<any> {
-    return this._contentIcon || this._icon;
-  }
-  set icon(icon: TemplateRef<any>) {
-    this._icon = icon;
-  }
-  private _icon: TemplateRef<any>;
-
-  /** @docs-private */
-  @HostBinding('class.sbb-button') buttonClass = true;
-
-  /** @docs-private */
-  @HostBinding('class.sbb-button-has-icon') get buttonHasIconClass() {
-    return !!this.icon;
-  }
-
-  /** @docs-private */
-  @ContentChild(ButtonIconDirective, { read: TemplateRef, static: false })
-  _contentIcon: TemplateRef<any>;
-
-  /** @docs-private */
-  @HostBinding('class.sbb-button-primary')
-  get _primaryClass() {
-    return this.mode === 'primary';
-  }
-
-  /** @docs-private */
-  @HostBinding('class.sbb-button-secondary')
-  get _secondaryClass() {
-    return this.mode === 'secondary';
-  }
-
-  /** @docs-private */
-  @HostBinding('class.sbb-button-ghost')
-  get _ghostClass() {
-    return this.mode === 'ghost';
-  }
 
   /** @docs-private */
   @HostBinding('class.sbb-button-frameless')
