b7ec7c05f39296e5d9250286f7275f3056cc2261
efux <efux@users.noreply.github.com>
2019-06-21 13:23:05 +0200

    feat(business/autocomplete): expose autocomplete component for business (#42)

--- projects/sbb-esta/angular-business/src/public-api.ts
+++ projects/sbb-esta/angular-business/src/public-api.ts
@@ -1,9 +1,10 @@
 /*
  * Public API Surface of angular-business
  */
+export * from './lib/autocomplete/autocomplete';
+export * from './lib/button/button';
 export * from './lib/field/field';
 export * from './lib/radio-button/radio-button';
 export * from './lib/select/select';
-export * from './lib/time-input/time-input';
 export * from './lib/textarea/textarea';
-export * from './lib/button/button';
+export * from './lib/time-input/time-input';
