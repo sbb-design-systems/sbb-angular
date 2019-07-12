ecd34d6acda02a1f1095dc9e27bdf94a21ecd92c
Lukas Spirig <lukas.spirig@sbb.ch>
2019-07-12 15:10:59 +0200

    refactor: deprecate KeycloakConfig.idpHint

--- projects/sbb-esta/angular-keycloak/src/lib/auth/keycloak-config.ts
+++ projects/sbb-esta/angular-keycloak/src/lib/auth/keycloak-config.ts
@@ -2,5 +2,8 @@ export interface KeycloakConfig {
   url?: string;
   realm?: string;
   clientId?: string;
+  /**
+   * @deprecated This is not used internally. Use the loginOptions parameter instead on AuthModule.forRoot.
+   */
   idpHint?: string;
 }
