413776d8cefe1b7216d9ef7dd52aeb7f9d5d026d
Lukas Spirig <lukas.spirig@gmail.ch>
2019-09-02 16:35:18 +0200

    Fix module-less elements

--- projects/angular-showcase/src/app/icons/icons.module.ts
+++ projects/angular-showcase/src/app/icons/icons.module.ts
@@ -1,10 +1,12 @@
 import { CommonModule } from '@angular/common';
 import { NgModule } from '@angular/core';
 
+import { ExamplesIconDocComponent } from './examples-icon-doc/examples-icon-doc.component';
+import { IconViewerDirective } from './icon-viewer.directive';
 import { IconsRoutingModule } from './icons-routing.module';
 
 @NgModule({
-  declarations: [],
+  declarations: [IconViewerDirective, ExamplesIconDocComponent],
   imports: [CommonModule, IconsRoutingModule]
 })
 export class IconsModule {}
