ff89e258597a9c206a7e94b5fe3e25126ea4c091
Danny <cricellidaniele@gmail.com>
2019-09-03 16:14:27 +0200

    feat(business/processflow): refactor for business-shocase

--- projects/angular-showcase/src/app/business/examples/examples.module.ts
+++ projects/angular-showcase/src/app/business/examples/examples.module.ts
@@ -1,8 +1,14 @@
 import { CommonModule } from '@angular/common';
 import { NgModule } from '@angular/core';
+import { ProcessflowShowcaseComponent } from './processflow-showcase/processflow-showcase.component';
+import { ProcessflowModule } from '@sbb-esta/angular-business';
+
+const exampleComponents = [ProcessflowShowcaseComponent];
 
 @NgModule({
-  declarations: [],
-  imports: [CommonModule]
+  declarations: exampleComponents,
+  entryComponents: exampleComponents,
+  exports: exampleComponents,
+  imports: [CommonModule, ProcessflowModule]
 })
 export class ExamplesModule {}
