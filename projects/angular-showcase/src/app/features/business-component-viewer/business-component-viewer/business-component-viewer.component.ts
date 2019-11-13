232450763c64ffa0814db125730d68f63d63d3e2
e503350 <e503350@sbb.ch>
2019-11-13 17:28:33 +0100

    feat(business/tabs): fix linting

--- projects/angular-showcase/src/app/features/business-component-viewer/business-component-viewer/business-component-viewer.component.ts
+++ projects/angular-showcase/src/app/features/business-component-viewer/business-component-viewer/business-component-viewer.component.ts
@@ -1,7 +1,7 @@
 import { Component, Renderer2, ViewChild } from '@angular/core';
 import { ActivatedRoute } from '@angular/router';
-
 import { TabsComponent } from '@sbb-esta/angular-business/tabs';
+
 import { ComponentViewerBase } from '../../../shared/component-viewer-base';
 import { ExampleProvider } from '../../../shared/example-provider';
 import { HtmlLoader } from '../../../shared/html-loader.service';
