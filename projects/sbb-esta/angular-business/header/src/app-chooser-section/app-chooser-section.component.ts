688bcf471ba681ef4d83f381a4d38519abbd8e1d
kyubisation <lukas.spirig@sbb.ch>
2019-11-22 14:55:10 +0100

    feat(business): implement responsive menus for header (#224)

--- projects/sbb-esta/angular-business/header/src/app-chooser-section/app-chooser-section.component.ts
+++ projects/sbb-esta/angular-business/header/src/app-chooser-section/app-chooser-section.component.ts
@@ -1,4 +1,6 @@
-import { ChangeDetectionStrategy, Component, Input } from '@angular/core';
+import { ChangeDetectionStrategy, Component, HostListener, Inject, Input } from '@angular/core';
+
+import { Header, SBB_HEADER } from '../header/header';
 
 @Component({
   selector: 'sbb-app-chooser-section',
@@ -8,4 +10,15 @@ import { ChangeDetectionStrategy, Component, Input } from '@angular/core';
 })
 export class AppChooserSectionComponent {
   @Input() label: string;
+
+  constructor(@Inject(SBB_HEADER) private _header: Header) {}
+
+  /** Close the header menu when any a or button child element is clicked. */
+  @HostListener('click', ['$event'])
+  _handleChildClick(event: Event) {
+    const target = event.target as HTMLElement;
+    if (target && target.tagName && (target.tagName === 'A' || target.tagName === 'BUTTON')) {
+      this._header.opened = false;
+    }
+  }
 }
