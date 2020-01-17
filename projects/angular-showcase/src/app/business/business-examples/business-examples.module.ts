e6e168b4fb6fe451a91fa5973a60529b15302f31
Lanz Valentina (IT-SWE-CC2-UI) <lanz.valentina@sbb.ch>
2020-01-17 14:37:21 +0100

    refactor(business): rebase master

--- projects/angular-showcase/src/app/business/business-examples/business-examples.module.ts
+++ projects/angular-showcase/src/app/business/business-examples/business-examples.module.ts
@@ -11,9 +11,8 @@ import { HeaderModule } from '@sbb-esta/angular-business/header';
 import { PaginationModule } from '@sbb-esta/angular-business/pagination';
 import { ProcessflowModule } from '@sbb-esta/angular-business/processflow';
 import { RadioButtonModule } from '@sbb-esta/angular-business/radio-button';
-import { TabsModule } from '@sbb-esta/angular-business/tabs';
-import { SbbSortModule } from '@sbb-esta/angular-business/table';
 import { TableModule } from '@sbb-esta/angular-business/table';
+import { TabsModule } from '@sbb-esta/angular-business/tabs';
 import { TooltipModule } from '@sbb-esta/angular-business/tooltip';
 import { UserMenuModule } from '@sbb-esta/angular-business/usermenu';
 import { IconCollectionModule } from '@sbb-esta/angular-icons';
@@ -29,24 +28,13 @@ import {
 import { PaginationShowcaseComponent } from './pagination-showcase/pagination-showcase.component';
 import { SimpleContextmenuComponent } from './simple-contextmenu/simple-contextmenu.component';
 import { SkippableProcessflowComponent } from './skippable-processflow/skippable-processflow.component';
-import { PersonListComponent } from './tabs-showcase/person/person-list/person-list.component';
-import { TabsShowcaseComponent } from './tabs-showcase/tabs-showcase.component';
-import {
-  TableShowcase1Component,
-  TableShowcase2Component,
-  TableShowcase3Component,
-  TableShowcaseComponent
-} from './table-showcase/table-showcase.component';
-import { TableActionsShowcaseComponent } from './table-showcase/table-actions-showcase/table-actions-showcase.component';
-import { TableGroupedColumnsShowcaseComponent } from './table-showcase/table-grouped-columns-showcase/table-grouped-columns-showcase.component';
-import { TableGroupedRowsShowcaseComponent } from './table-showcase/table-grouped-rows-showcase/table-grouped-rows-showcase.component';
-import { TableSimpleShowcaseComponent } from './table-showcase/table-simple-showcase/table-simple-showcase.component';
-import { TableSortShowcaseComponent } from './table-showcase/table-sort-showcase/table-sort-showcase.component';
 import { ActionsTableComponent } from './table-showcase/actions-table/actions-table.component';
 import { GroupedColumnsTableComponent } from './table-showcase/grouped-columns-table/grouped-columns-table.component';
 import { GroupedRowsTableComponent } from './table-showcase/grouped-rows-table/grouped-rows-table.component';
 import { SimpleTableComponent } from './table-showcase/simple-table/simple-table.component';
 import { SortableTableComponent } from './table-showcase/sortable-table/sortable-table.component';
+import { PersonListComponent } from './tabs-showcase/person/person-list/person-list.component';
+import { TabsShowcaseComponent } from './tabs-showcase/tabs-showcase.component';
 import { TooltipShowcaseComponent } from './tooltip-showcase/tooltip-showcase.component';
 import { UsermenuShowcaseComponent } from './usermenu-showcase/usermenu-showcase.component';
 
@@ -55,9 +43,6 @@ const exampleComponents = [
   SkippableProcessflowComponent,
   TooltipShowcaseComponent,
   UsermenuShowcaseComponent,
-  TableShowcaseComponent,
-  TableShowcase1Component,
-  TableShowcase2Component,
   PersonListComponent,
   TabsShowcaseComponent,
   UsermenuShowcaseComponent,
@@ -68,13 +53,6 @@ const exampleComponents = [
   DialogShowcaseExample2ContentComponent,
   DialogShowcaseExample3Component,
   PaginationShowcaseComponent,
-  TableShowcase2Component,
-  TableShowcase3Component,
-  TableSimpleShowcaseComponent,
-  TableActionsShowcaseComponent,
-  TableGroupedRowsShowcaseComponent,
-  TableSortShowcaseComponent,
-  TableGroupedColumnsShowcaseComponent,
   SimpleTableComponent,
   ActionsTableComponent,
   GroupedRowsTableComponent,
