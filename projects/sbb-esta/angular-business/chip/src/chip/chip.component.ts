5d8b07dcb96ab614e16f18ff481f5f56fa5652e7
Lanz Valentina (IT-SWE-CC2-UI) <lanz.valentina@sbb.ch>
2020-01-27 11:38:19 +0100

    feat(business): implement chip component
    implement dismiss functionality

--- projects/sbb-esta/angular-business/chip/src/chip/chip.component.ts
+++ projects/sbb-esta/angular-business/chip/src/chip/chip.component.ts
@@ -1,4 +1,4 @@
-import { Component, HostBinding, Input } from '@angular/core';
+import { Component, EventEmitter, HostBinding, Input, Output } from '@angular/core';
 
 @Component({
   selector: 'sbb-chip',
@@ -12,11 +12,26 @@ export class ChipComponent {
   @Input()
   disabled = false;
 
+  @Output()
+  dismissed: EventEmitter<boolean> = new EventEmitter();
+
+  @HostBinding('attr.aria-hidden') ariaHidden: 'false' | 'true';
+
+  @HostBinding('hidden')
+  get hidden() {
+    return this.ariaHidden === 'true';
+  }
+
   @HostBinding('class.sbb-chip-disabled') get isDisabled() {
     return !!this.disabled;
   }
 
   @HostBinding('class.sbb-chip-active') get isActive() {
     return !this.disabled;
+  }
+
+  dismiss() {
+    this.ariaHidden = 'true';
+    this.dismissed.emit(false);
   }
 }
