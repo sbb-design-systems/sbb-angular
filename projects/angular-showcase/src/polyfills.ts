3492927ebe5c3708800edfa61e6c7ba30987eb79
u204068 <u204068@sbb.ch>
2020-02-04 15:37:10 +0100

    fix: order of import

--- projects/angular-showcase/src/polyfills.ts
+++ projects/angular-showcase/src/polyfills.ts
@@ -1,7 +1,3 @@
-/***************************************************************************************************
- * Load `$localize` onto the global scope - used if i18n tags appear in Angular templates.
- */
-import '@angular/localize/init';
 /**
  * This file includes polyfills needed by Angular and is loaded before the app.
  * You can add your own extra polyfills to this file.
@@ -68,3 +64,8 @@ import 'zone.js/dist/zone'; // Included with Angular CLI.
 // tslint:disable-next-line: ordered-imports
 import 'core-js/es6/array';
 import 'core-js/es6/string';
+
+/***************************************************************************************************
+ * Load `$localize` onto the global scope - used if i18n tags appear in Angular templates.
+ */
+import '@angular/localize/init';
