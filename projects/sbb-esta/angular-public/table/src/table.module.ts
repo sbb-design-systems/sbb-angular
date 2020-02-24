2151aa465d4ae64f6135d505c2d0aa0bd7522ccb
Lukas Spirig <lukas.spirig@gmail.ch>
2020-02-24 15:09:19 +0100

    refactor(public): remove obsolete table helper directive

--- projects/sbb-esta/angular-public/table/src/table.module.ts
+++ projects/sbb-esta/angular-public/table/src/table.module.ts
@@ -1,12 +1,11 @@
 import { CommonModule } from '@angular/common';
 import { NgModule } from '@angular/core';
 
-import { ScrollAreaDirective } from './table/table-scroll-area.directive';
 import { TableComponent } from './table/table.component';
 
 @NgModule({
   imports: [CommonModule],
-  declarations: [TableComponent, ScrollAreaDirective],
-  exports: [TableComponent, ScrollAreaDirective]
+  declarations: [TableComponent],
+  exports: [TableComponent]
 })
 export class TableModule {}
