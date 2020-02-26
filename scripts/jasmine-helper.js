693d53c98312db9c5cdb73591cdda6050d5852cc
Lukas Spirig <lukas.spirig@gmail.ch>
2020-02-26 11:32:54 +0100

    feat: add migration for public and business packages
    This will allow a consumer to run `ng update @sbb-esta/angular-public` or `ng update @sbb-esta/angular-business`, which will add @sbb-esta/angular-core to the project and also install it.

---
+++ scripts/jasmine-helper.js
@@ -0,0 +1,6 @@
+const { compilerOptions } = require('../schematics/tsconfig.json');
+
+require('ts-node').register({
+  typeCheck: true,
+  compilerOptions: { ...compilerOptions, module: 'commonjs' }
+});
