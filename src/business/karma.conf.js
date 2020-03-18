f6cbc204b4ca5e16ad1c8af153eaebb9972f0235
Lukas Spirig <lukas.spirig@gmail.ch>
2020-03-18 14:55:48 +0100

    refactor: clean files

--- src/business/karma.conf.js
+++
@@ -1,4 +0,0 @@
-// Karma configuration file, see link for more information
-// https://karma-runner.github.io/1.0/config/configuration-file.html
-
-module.exports = require('../../../scripts/karma.library')('angular-business');
