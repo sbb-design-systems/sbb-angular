da23cf564b58e54cdff54c8640683b6c11704544
jeripeierSBB <jeremias.peier@sbb.ch>
2020-03-19 11:26:09 +0100

    refactor: fold tests

--- projects/sbb-esta/angular-business/chip/src/chip-input/chip-input.component.spec.ts
+++ projects/sbb-esta/angular-business/chip/src/chip-input/chip-input.component.spec.ts
@@ -47,99 +47,108 @@ class ChipInputReactiveFormsTestComponent {
 }
 
 describe('ChipInputComponent', () => {
-  let component: ChipInputReactiveFormsTestComponent;
-  let fixture: ComponentFixture<ChipInputReactiveFormsTestComponent>;
-  let inputElement: HTMLInputElement;
+  describe('reactive forms', () => {
+    let component: ChipInputReactiveFormsTestComponent;
+    let fixture: ComponentFixture<ChipInputReactiveFormsTestComponent>;
+    let inputElement: HTMLInputElement;
 
-  configureTestSuite(() => {
-    TestBed.configureTestingModule({
-      declarations: [ChipInputComponent, ChipComponent, ChipInputReactiveFormsTestComponent],
-      imports: [CommonModule, AutocompleteModule, IconCrossModule, ReactiveFormsModule, FieldModule]
+    configureTestSuite(() => {
+      TestBed.configureTestingModule({
+        declarations: [ChipInputComponent, ChipComponent, ChipInputReactiveFormsTestComponent],
+        imports: [
+          CommonModule,
+          AutocompleteModule,
+          IconCrossModule,
+          ReactiveFormsModule,
+          FieldModule
+        ]
+      });
     });
-  });
 
-  beforeEach(() => {
-    fixture = TestBed.createComponent(ChipInputReactiveFormsTestComponent);
-    component = fixture.componentInstance;
-    fixture.detectChanges();
-    inputElement = fixture.debugElement.query(By.css('input.sbb-chip-input-textfield'))
-      .nativeElement;
-  });
+    beforeEach(() => {
+      fixture = TestBed.createComponent(ChipInputReactiveFormsTestComponent);
+      component = fixture.componentInstance;
+      fixture.detectChanges();
+      inputElement = fixture.debugElement.query(By.css('input.sbb-chip-input-textfield'))
+        .nativeElement;
+    });
 
-  it('should create', () => {
-    expect(component).toBeTruthy();
-  });
+    it('should create', () => {
+      expect(component).toBeTruthy();
+    });
 
-  it('should contain one option chip through preselection', () => {
-    fixture.whenStable().then(() => {
+    it('should contain one option chip through preselection', done => {
+      fixture.whenStable().then(() => {
+        const chipComponents = fixture.debugElement.queryAll(By.directive(ChipComponent));
+        expect(chipComponents.length).toBe(1);
+        done();
+      });
+    });
+
+    it('should contain two option chips using setValue', () => {
+      component.formGroup.get('chip').setValue(['option-1', 'option-2']);
+      fixture.detectChanges();
+
       const chipComponents = fixture.debugElement.queryAll(By.directive(ChipComponent));
-      expect(chipComponents.length).toBe(1);
+      expect(chipComponents.length).toBe(2);
     });
-  });
 
-  it('should contain two option chips using setValue', () => {
-    component.formGroup.get('chip').setValue(['option-1', 'option-2']);
-    fixture.detectChanges();
+    it('should hide form error if invalid but untouched', () => {
+      component.formGroup.get('chip').setValue([]);
+      fixture.detectChanges();
 
-    const chipComponents = fixture.debugElement.queryAll(By.directive(ChipComponent));
-    expect(chipComponents.length).toBe(2);
-  });
+      const errorText = fixture.debugElement.query(By.directive(FormErrorDirective));
+      expect(component.formGroup.get('chip').invalid).toBe(true);
+      expect(errorText).toBeFalsy();
+    });
 
-  it('should hide form error if invalid but untouched', () => {
-    component.formGroup.get('chip').setValue([]);
-    fixture.detectChanges();
+    it('should display form error when input was touched', () => {
+      component.formGroup.get('chip').setValue([]);
+      fixture.detectChanges();
 
-    const errorText = fixture.debugElement.query(By.directive(FormErrorDirective));
-    expect(component.formGroup.get('chip').invalid).toBe(true);
-    expect(errorText).toBeFalsy();
-  });
+      inputElement.focus();
 
-  it('should display form error when input was touched', () => {
-    component.formGroup.get('chip').setValue([]);
-    fixture.detectChanges();
+      fixture.detectChanges();
+      expect(fixture.debugElement.query(By.directive(FormErrorDirective))).toBeFalsy();
+      dispatchFakeEvent(inputElement, 'blur');
 
-    inputElement.focus();
+      fixture.detectChanges();
+      expect(fixture.debugElement.query(By.directive(FormErrorDirective))).toBeTruthy();
+    });
 
-    fixture.detectChanges();
-    expect(fixture.debugElement.query(By.directive(FormErrorDirective))).toBeFalsy();
-    dispatchFakeEvent(inputElement, 'blur');
+    it('should disable chip input and chips', () => {
+      component.formGroup.get('chip').disable();
+      const chipInputComponent = fixture.debugElement.query(By.directive(ChipInputComponent));
+      const chipComponents = fixture.debugElement.queryAll(By.directive(ChipComponent));
+      fixture.detectChanges();
 
-    fixture.detectChanges();
-    expect(fixture.debugElement.query(By.directive(FormErrorDirective))).toBeTruthy();
-  });
+      chipComponents.forEach(chipComponent =>
+        expect(chipComponent.classes['sbb-chip-disabled']).toBe(true)
+      );
+      expect(chipInputComponent.classes['sbb-chip-input-disabled']).toBe(true);
+    });
 
-  it('should disable chip input and chips', () => {
-    component.formGroup.get('chip').disable();
-    const chipInputComponent = fixture.debugElement.query(By.directive(ChipInputComponent));
-    const chipComponents = fixture.debugElement.queryAll(By.directive(ChipComponent));
-    fixture.detectChanges();
+    it('should forward focus when clicking sbb-field label', () => {
+      const label = fixture.debugElement.query(By.css('label'));
+      spyOn(inputElement, 'focus');
 
-    chipComponents.forEach(chipComponent =>
-      expect(chipComponent.classes['sbb-chip-disabled']).toBe(true)
-    );
-    expect(chipInputComponent.classes['sbb-chip-input-disabled']).toBe(true);
-  });
+      expect(inputElement.focus).not.toHaveBeenCalled();
+      dispatchMouseEvent(label.nativeElement, 'click');
+      fixture.detectChanges();
 
-  it('should forward focus when clicking sbb-field label', () => {
-    const label = fixture.debugElement.query(By.css('label'));
-    spyOn(inputElement, 'focus');
+      expect(inputElement.focus).toHaveBeenCalled();
+    });
 
-    expect(inputElement.focus).not.toHaveBeenCalled();
-    dispatchMouseEvent(label.nativeElement, 'click');
-    fixture.detectChanges();
+    it('should correctly update chip value by using keyboard', () => {
+      expect(component.formGroup.get('chip').value).toEqual(['option-1']);
 
-    expect(inputElement.focus).toHaveBeenCalled();
-  });
+      inputElement.focus();
+      typeInElement(inputElement, 'option-2');
+      dispatchEvent(inputElement, createKeyboardEvent('keydown', ENTER, 'Enter', inputElement));
 
-  it('should correctly update chip value by using keyboard', () => {
-    expect(component.formGroup.get('chip').value).toEqual(['option-1']);
+      fixture.detectChanges();
 
-    inputElement.focus();
-    typeInElement(inputElement, 'option-2');
-    dispatchEvent(inputElement, createKeyboardEvent('keydown', ENTER, 'Enter', inputElement));
-
-    fixture.detectChanges();
-
-    expect(component.formGroup.get('chip').value).toEqual(['option-1', 'option-2']);
+      expect(component.formGroup.get('chip').value).toEqual(['option-1', 'option-2']);
+    });
   });
 });
