55f39a71130910d267e08418cc8342b71add09f6
Lukas Spirig <lukas.spirig@gmail.ch>
2020-03-19 16:06:39 +0100

    refactor: move auth files

--- src/keycloak/public-api.ts
+++ src/keycloak/public-api.ts
@@ -2,4 +2,9 @@
  * Public API Surface of angular-keycloak
  */
 
-export * from './src/lib/auth/auth';
+export * from './src/auth.interceptor';
+export * from './src/auth.module';
+export * from './src/auth.service';
+export * from './src/auth.tokens';
+export * from './src/keycloak-config';
+
