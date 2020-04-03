8f7a477912700ee5b09b2b4214a2553bab9e8022
jeripeierSBB <jeremias.peier@sbb.ch>
2020-04-03 13:42:56 +0200

    refactor: add type to SBB_BUSINESS_DATE_ADAPTER

--- projects/sbb-esta/angular-core/datetime/src/business-date-adapter-provider.ts
+++ projects/sbb-esta/angular-core/datetime/src/business-date-adapter-provider.ts
@@ -1,4 +1,9 @@
+import { Provider } from '@angular/core';
+
 import { BusinessDateAdapter } from './business-date-adapter';
 import { DateAdapter } from './date-adapter';
 
-export const SBB_BUSINESS_DATE_ADAPTER = { provide: DateAdapter, useClass: BusinessDateAdapter };
+export const SBB_BUSINESS_DATE_ADAPTER: Provider = {
+  provide: DateAdapter,
+  useClass: BusinessDateAdapter
+};
