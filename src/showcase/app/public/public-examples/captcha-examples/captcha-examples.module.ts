a6d14e51960cc1e3c4740ec5b402a35cdeba5f66
jeripeierSBB <jeremias.peier@sbb.ch>
2020-04-21 16:54:49 +0200

    fix: add ButtonModule to captcha example

--- src/showcase/app/public/public-examples/captcha-examples/captcha-examples.module.ts
+++ src/showcase/app/public/public-examples/captcha-examples/captcha-examples.module.ts
@@ -1,6 +1,7 @@
 import { CommonModule } from '@angular/common';
 import { NgModule } from '@angular/core';
 import { FormsModule, ReactiveFormsModule } from '@angular/forms';
+import { ButtonModule } from '@sbb-esta/angular-public/button';
 import { CaptchaModule } from '@sbb-esta/angular-public/captcha';
 
 import { CaptchaExampleComponent } from './captcha-example/captcha-example.component';
@@ -8,7 +9,7 @@ import { CaptchaExampleComponent } from './captcha-example/captcha-example.compo
 const EXAMPLES = [CaptchaExampleComponent];
 
 @NgModule({
-  imports: [CommonModule, FormsModule, ReactiveFormsModule, CaptchaModule],
+  imports: [CommonModule, FormsModule, ReactiveFormsModule, CaptchaModule, ButtonModule],
   declarations: EXAMPLES,
   exports: EXAMPLES
 })
