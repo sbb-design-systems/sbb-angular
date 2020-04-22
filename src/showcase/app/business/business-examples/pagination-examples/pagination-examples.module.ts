b6baa62674e2a1f652c636ebe15494e1aa4ce2c3
jeripeierSBB <jeremias.peier@sbb.ch>
2020-04-22 15:06:11 +0200

    fix: add ButtonModule to pagination business example

--- src/showcase/app/business/business-examples/pagination-examples/pagination-examples.module.ts
+++ src/showcase/app/business/business-examples/pagination-examples/pagination-examples.module.ts
@@ -3,13 +3,21 @@ import { NgModule } from '@angular/core';
 import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 import { FieldModule } from '@sbb-esta/angular-business/field';
 import { PaginationModule } from '@sbb-esta/angular-business/pagination';
+import { ButtonModule } from '@sbb-esta/angular-public/button';
 
 import { PaginationExampleComponent } from './pagination-example/pagination-example.component';
 
 const EXAMPLES = [PaginationExampleComponent];
 
 @NgModule({
-  imports: [CommonModule, FormsModule, ReactiveFormsModule, FieldModule, PaginationModule],
+  imports: [
+    CommonModule,
+    FormsModule,
+    ReactiveFormsModule,
+    FieldModule,
+    PaginationModule,
+    ButtonModule
+  ],
   declarations: EXAMPLES,
   exports: EXAMPLES
 })
