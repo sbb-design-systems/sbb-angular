2f39da321b07b4fa4b5c0e70e30d7133e8e26c42
jeripeierSBB <jeremias.peier@sbb.ch>
2020-05-08 15:38:29 +0200

    refactor: public textexpand example

--- src/showcase/app/public/public-examples/textexpand-examples/textexpand-example/textexpand-example.component.ts
+++ src/showcase/app/public/public-examples/textexpand-examples/textexpand-example/textexpand-example.component.ts
@@ -2,7 +2,6 @@ import { Component } from '@angular/core';
 
 @Component({
   selector: 'sbb-textexpand-example',
-  templateUrl: './textexpand-example.component.html',
-  styleUrls: ['./textexpand-example.component.css']
+  templateUrl: './textexpand-example.component.html'
 })
 export class TextexpandExampleComponent {}
