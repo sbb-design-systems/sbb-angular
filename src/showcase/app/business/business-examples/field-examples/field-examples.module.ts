be319561cb766f02991968c3d7e6dc9dca1b5082
Lanz Valentina (IT-SWE-CC2-UI) <17550954+vlanz@users.noreply.github.com>
2020-05-26 07:51:27 +0200

    refactor(showcase): optimize imports
    prefer secondary entrypoints for component import

--- src/showcase/app/business/business-examples/field-examples/field-examples.module.ts
+++ src/showcase/app/business/business-examples/field-examples/field-examples.module.ts
@@ -1,13 +1,11 @@
 import { CommonModule } from '@angular/common';
 import { NgModule } from '@angular/core';
 import { FormsModule, ReactiveFormsModule } from '@angular/forms';
-import {
-  ButtonModule,
-  CheckboxModule,
-  DatepickerModule,
-  FieldModule,
-  SelectModule,
-} from '@sbb-esta/angular-business';
+import { ButtonModule } from '@sbb-esta/angular-business/button';
+import { CheckboxModule } from '@sbb-esta/angular-business/checkbox';
+import { DatepickerModule } from '@sbb-esta/angular-business/datepicker';
+import { FieldModule } from '@sbb-esta/angular-business/field';
+import { SelectModule } from '@sbb-esta/angular-business/select';
 
 import { FieldExampleComponent } from './field-example/field-example.component';
 
--- src/showcase/app/public/public-examples/field-examples/field-examples.module.ts
+++ src/showcase/app/public/public-examples/field-examples/field-examples.module.ts
@@ -1,13 +1,11 @@
 import { CommonModule } from '@angular/common';
 import { NgModule } from '@angular/core';
 import { FormsModule, ReactiveFormsModule } from '@angular/forms';
-import {
-  ButtonModule,
-  CheckboxModule,
-  DatepickerModule,
-  FieldModule,
-  SelectModule,
-} from '@sbb-esta/angular-public';
+import { ButtonModule } from '@sbb-esta/angular-public/button';
+import { CheckboxModule } from '@sbb-esta/angular-public/checkbox';
+import { DatepickerModule } from '@sbb-esta/angular-public/datepicker';
+import { FieldModule } from '@sbb-esta/angular-public/field';
+import { SelectModule } from '@sbb-esta/angular-public/select';
 
 import { FieldExampleComponent } from './field-example/field-example.component';
 
