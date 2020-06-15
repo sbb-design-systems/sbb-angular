d5a2555ec32b79228024b6e8c17f543a2809bf66
Lanz Valentina (IT-SWE-CC2-UI) <17550954+vlanz@users.noreply.github.com>
2020-06-15 16:32:50 +0200

    feat(business): implement notification as toast
    implement notification component similarly to material toast

--- src/angular-business/notification/public-api.ts
+++ src/angular-business/notification/public-api.ts
@@ -1,4 +1,6 @@
 export * from './notification.module';
-// TODO: Deprecate NotificationsModule module usage (Only NotificationModule should be supported)
-export { NotificationModule as NotificationsModule } from './notification.module';
+export * from './notification/notification.service';
+export * from './notification-container/notification-container.component';
+export * from './notification/notification-ref';
+export * from './notification/notification-config';
 export * from './notification/notification.component';
