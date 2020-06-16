ce16d48308cd795a5552220e7deb7ac13b3b5086
Lukas Spirig <lukas.spirig@gmail.ch>
2020-06-16 17:45:54 +0200

    feat: implement initial version of schematics

--- src/angular-core/icon/icon-module.ts
+++ src/angular-core/icon/icon-module.ts
@@ -1,18 +1,24 @@
 import { ModuleWithProviders, NgModule } from '@angular/core';
 
 import { SbbIconComponent } from './icon';
-import { SBB_MODULE_ICONS, SBB_MODULE_ICON_REGISTRY_PROVIDER } from './icon-cdn-registry';
+import { SBB_MODULE_ICONS_TOKEN, SBB_MODULE_ICON_REGISTRY_PROVIDER } from './module-icons';
 
 @NgModule({
   declarations: [SbbIconComponent],
   exports: [SbbIconComponent],
 })
 export class SbbIconModule {
-  static forInternalModule(icons: string[]): ModuleWithProviders {
+  /**
+   * WARNING!
+   * Internal usage only! Not intended for consumers!
+   * @param icons The icons to register for the consuming module.
+   * @docs-private
+   */
+  static ɵforInternalModule(icons: string[]): ModuleWithProviders {
     return {
       ngModule: SbbIconModule,
       providers: [
-        { provide: SBB_MODULE_ICONS, useValue: icons },
+        { provide: SBB_MODULE_ICONS_TOKEN, useValue: icons },
         SBB_MODULE_ICON_REGISTRY_PROVIDER,
       ],
     };
