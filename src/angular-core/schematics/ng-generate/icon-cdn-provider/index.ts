3e9b1985978514e1200fcfa9e9c9743efdc9de53
Lukas Spirig <lukas.spirig@gmail.ch>
2020-06-17 14:34:17 +0200

    feat: finish icon-cdn-provider schematics

--- src/angular-core/schematics/ng-generate/icon-cdn-provider/index.ts
+++ src/angular-core/schematics/ng-generate/icon-cdn-provider/index.ts
@@ -1,5 +1,8 @@
+import { WorkspaceProject } from '@angular-devkit/core/src/experimental/workspace';
 import {
   apply,
+  FileEntry,
+  forEach,
   mergeWith,
   move,
   Rule,
@@ -10,7 +13,7 @@ import {
 } from '@angular-devkit/schematics';
 import { getProjectFromWorkspace } from '@angular/cdk/schematics';
 import { getWorkspace } from '@schematics/angular/utility/config';
-import { join } from 'path';
+import { ProjectType } from '@schematics/angular/utility/workspace-models';
 
 import { Schema as IconCdnProviderOptions } from './schema';
 export { Schema as IconCdnProviderOptions } from './schema';
@@ -24,32 +27,55 @@ export function iconCdnProvider(options: IconCdnProviderOptions): Rule {
   return async (tree: Tree, context: SchematicContext): Promise<Rule> => {
     const workspace = getWorkspace(tree);
     const project = getProjectFromWorkspace(workspace, options.project);
-    const targetDir = join(project.root, options.targetDir);
-    const cdnIndex: CdnIndexResponse = await downloadIndex().catch(() => {
-      context.logger.warn(
-        `Unable to resolve ${options.cdnIndexUrl}. Falling back to empty configuration.`
-      );
-      return { version: 'local', icons: [] } as CdnIndexResponse;
-    });
+    const path = options.path || buildDefaultPath(project as any);
+    const cdnIndex = await downloadIndex()
+      .then((r) => {
+        context.logger.info(
+          `Retrieved ${r.icons.length} icons with version ${r.version} from ${options.cdnIndexUrl}`
+        );
+        return r;
+      })
+      .catch(() => {
+        context.logger.warn(
+          `Unable to resolve ${options.cdnIndexUrl}. Falling back to empty configuration.`
+        );
+        return { version: 'local', icons: [] } as CdnIndexResponse;
+      });
 
     return mergeWith(
       apply(url('./files/provider'), [
         template({ cdnIndex, cdnBaseUrl: options.cdnBaseUrl }),
-        move(targetDir),
+        move(path),
+        forEach((fileEntry: FileEntry) => {
+          // Just by adding this is allows the file to be overwritten if it already exists
+          if (tree.exists(fileEntry.path)) {
+            tree.overwrite(fileEntry.path, fileEntry.content);
+            return null;
+          }
+          return fileEntry;
+        }),
       ])
     );
   };
+
+  /**
+   * Build a default project path for generating.
+   * @param project The project to build the path for.
+   */
+  function buildDefaultPath(project: WorkspaceProject): string {
+    const root = project.sourceRoot ? `/${project.sourceRoot}/` : `/${project.root}/src/`;
+
+    const projectDirName = project.projectType === ProjectType.Application ? 'app' : 'lib';
+
+    return `${root}${projectDirName}`;
+  }
 
   function downloadIndex() {
     return new Promise<CdnIndexResponse>((resolve, reject) => {
       const { get } = options.cdnIndexUrl.startsWith('https') ? require('https') : require('http');
       get(options.cdnIndexUrl, (res: any) => {
         let body = '';
-
-        res.on('data', (chunk: any) => {
-          body += chunk;
-        });
-
+        res.on('data', (chunk: any) => (body += chunk));
         res.on('end', () => {
           try {
             resolve(JSON.parse(body));
