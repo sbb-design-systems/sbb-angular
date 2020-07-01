01e140850464e0cb52ad52c277e8b5648ef33a5a
jeripeierSBB <jeremias.peier@sbb.ch>
2020-07-01 18:02:52 +0200

    docs: how to update guide

---
+++ src/showcase/app/howtoupdate/how-to-update.component.ts
@@ -0,0 +1,15 @@
+import { Component } from '@angular/core';
+import { FormControl, FormGroup } from '@angular/forms';
+
+// @ts-ignore versions.ts is generated automatically by bazel
+import { libraryVersion } from '../versions';
+
+@Component({
+  selector: 'sbb-how-to-update',
+  templateUrl: './how-to-update.component.html',
+})
+export class HowToUpdateComponent {
+  libraryMajorVersion: number = +libraryVersion.split('.')[0];
+  libraryPredecessorMajorVersion = this.libraryMajorVersion - 1;
+  form: FormGroup = new FormGroup({ fullCommand: new FormControl(false) });
+}
