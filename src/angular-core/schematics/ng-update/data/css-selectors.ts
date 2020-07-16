4e913df617692e566b30099f7d4207278ca7d7be
Lukas Spirig <lukas.spirig@sbb.ch>
2020-07-16 16:48:45 +0200

    feat(core): implement icon migration schematic (#511)

---
+++ src/angular-core/schematics/ng-update/data/css-selectors.ts
@@ -0,0 +1,19 @@
+import { VersionChanges } from '@angular/cdk/schematics';
+
+export interface SbbAngularCssSelectorData {
+  /** The CSS selector to replace. */
+  replace: string;
+  /** The new CSS selector. */
+  replaceWith: string;
+  /** Whitelist where this replacement is made. If omitted it is made in all files. */
+  whitelist?: {
+    /** Replace this name in stylesheet files. */
+    stylesheet?: boolean;
+    /** Replace this name in HTML files. */
+    html?: boolean;
+    /** Replace this name in TypeScript strings. */
+    strings?: boolean;
+  };
+}
+
+export const cssSelectors: VersionChanges<SbbAngularCssSelectorData> = {};
--- src/angular-core/schematics/ng-update/index.ts
+++ src/angular-core/schematics/ng-update/index.ts
@@ -1,7 +1,44 @@
 import { Rule, SchematicContext, Tree } from '@angular-devkit/schematics';
+import { createMigrationSchematicRule, TargetVersion } from '@angular/cdk/schematics';
 
-export function updateToV9(): Rule {
-  return (host: Tree, _context: SchematicContext) => {
-    return host;
-  };
+import { IconMigration } from './migrations/icon-migration';
+import { sbbAngularUpgradeData } from './upgrade-data';
+
+/** Entry point for the migration schematics with target of sbb-angular 10.0.0 */
+export function updateToV10(): Rule {
+  return createMigrationSchematicRule(
+    TargetVersion.V10,
+    [],
+    sbbAngularUpgradeData,
+    onMigrationComplete
+  );
+}
+
+/** Entry point for the migration schematics with target of sbb-angular 11.0.0 */
+export function updateToV11(): Rule {
+  return createMigrationSchematicRule(
+    // TODO: Adapt for TargetVersion.V11
+    'version 11' as TargetVersion,
+    [IconMigration],
+    sbbAngularUpgradeData,
+    onMigrationComplete
+  );
+}
+
+/** Function that will be called when the migration completed. */
+function onMigrationComplete(
+  context: SchematicContext,
+  targetVersion: TargetVersion,
+  hasFailures: boolean
+) {
+  context.logger.info('');
+  context.logger.info(`  ✓  Updated @sbb-esta/angular-core to ${targetVersion}`);
+  context.logger.info('');
+
+  if (hasFailures) {
+    context.logger.warn(
+      '  ⚠  Some issues were detected but could not be fixed automatically. Please check the ' +
+        'output above and fix these issues manually.'
+    );
+  }
 }
