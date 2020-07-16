95d03ed3a34db4fe9c8d3318f29d1c0cade0b389
jeripeierSBB <jeremias.peier@sbb.ch>
2020-07-16 17:58:29 +0200

    refactor: checkbox panel examples

---
+++ src/showcase/app/public/public-examples/checkbox-panel-examples/checkbox-panel-icon-example/checkbox-panel-icon-example.component.ts
@@ -0,0 +1,7 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'sbb-checkbox-panel-icon-example',
+  templateUrl: './checkbox-panel-icon-example.component.html',
+})
+export class CheckboxPanelIconExampleComponent {}
