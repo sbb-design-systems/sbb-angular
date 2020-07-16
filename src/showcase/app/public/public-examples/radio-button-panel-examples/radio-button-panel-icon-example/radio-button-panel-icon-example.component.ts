ef86944c5189d7bc4f31b9d054a31fba1361157a
jeripeierSBB <jeremias.peier@sbb.ch>
2020-07-16 17:45:08 +0200

    refactor: radio button panel examples

---
+++ src/showcase/app/public/public-examples/radio-button-panel-examples/radio-button-panel-icon-example/radio-button-panel-icon-example.component.ts
@@ -0,0 +1,7 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'sbb-radio-button-panel-icon-example',
+  templateUrl: './radio-button-panel-icon-example.component.html',
+})
+export class RadioButtonPanelIconExampleComponent {}
---
+++ src/showcase/app/public/public-examples/radio-button-panel-examples/radio-button-panel-img-example/radio-button-panel-img-example.component.ts
@@ -0,0 +1,9 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'sbb-radio-button-panel-img-example',
+  templateUrl: './radio-button-panel-img-example.component.html',
+})
+export class RadioButtonPanelImgExampleComponent {
+  disabled: boolean;
+}
