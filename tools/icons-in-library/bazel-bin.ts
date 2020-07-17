74809046c5e2f225d35d3830c3597383651e124b
Lukas Spirig <lukas.spirig@sbb.ch>
2020-07-17 16:06:55 +0200

    fix: used icon detection should work as intended (#525)

--- tools/icons-in-library/bazel-bin.ts
+++ tools/icons-in-library/bazel-bin.ts
@@ -1,48 +1,34 @@
-import { readdirSync, readFileSync, writeFileSync } from 'fs';
-import { basename, join } from 'path';
+import { readFileSync, writeFileSync } from 'fs';
 
 if (require.main === module) {
-  const [outputFile, baseDir] = process.argv.slice(2);
-
-  const icons: { module: string; icons: string[] }[] = [];
-  const packages = readdirSync(baseDir, { withFileTypes: true })
-    .filter((d) => d.isDirectory() && d.name !== 'showcase')
-    .map((d) => join(baseDir, d.name));
-  for (const pckg of packages) {
-    const modules = readdirSync(pckg, { withFileTypes: true })
-      .filter((d) => d.isDirectory())
-      .map((d) => join(pckg, d.name));
-    for (const module of modules) {
-      const moduleIcons = findHtmlFiles(module)
-        .map((f) => readFileSync(f, 'utf8'))
-        .map(findIcons)
-        .reduce((current, next) => current.concat(next), [])
-        .filter((v, i, a) => a.indexOf(v) === i)
-        .sort();
-      if (moduleIcons.length) {
-        icons.push({
-          module: `@sbb-esta/${basename(pckg)}/${basename(module)}`,
-          icons: moduleIcons,
-        });
+  const [sourceFileManifest, outputFile] = process.argv.slice(2);
+  const moduleIcons: { [module: string]: string[] } = {};
+  readFileSync(sourceFileManifest, 'utf8')
+    .split(' ')
+    .map((filePath) => filePath.trim())
+    .filter((s) => s.endsWith('.html'))
+    .forEach((file) => {
+      const moduleName = `@sbb-esta/${file.match(/src\/(angular-\w+\/[\w-]+)/)![1]}`;
+      const content = readFileSync(file, 'utf8');
+      const detectedIcons = findIcons(content);
+      if (!detectedIcons.length) {
+        return;
+      } else if (!(moduleName in moduleIcons)) {
+        moduleIcons[moduleName] = [];
       }
-    }
-  }
 
+      moduleIcons[moduleName].push(...detectedIcons);
+    });
+
+  const icons = Object.keys(moduleIcons).reduce(
+    (collection, module) =>
+      collection.concat({
+        module,
+        icons: moduleIcons[module].filter((v, i, a) => a.indexOf(v) === i).sort(),
+      }),
+    [] as { module: string; icons: string[] }[]
+  );
   writeFileSync(outputFile, JSON.stringify(icons, null, 2), 'utf8');
-}
-
-function findHtmlFiles(root: string): string[] {
-  return readdirSync(root, { withFileTypes: true })
-    .map((d) => {
-      if (d.isFile() && d.name.endsWith('.html')) {
-        return [join(root, d.name)];
-      } else if (d.isDirectory()) {
-        return findHtmlFiles(join(root, d.name));
-      } else {
-        return [];
-      }
-    })
-    .reduce((current, next) => current.concat(next), []);
 }
 
 function findIcons(content: string) {
