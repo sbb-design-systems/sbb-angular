2726b97ad633d94d4ea24510430e8e1cc4711201
Lukas Spirig <lukas.spirig@sbb.ch>
2020-07-21 11:59:48 +0200

    refactor: contextmenu icon (#530)

--- src/angular-business/contextmenu/contextmenu.module.ts
+++ src/angular-business/contextmenu/contextmenu.module.ts
@@ -1,12 +1,12 @@
 import { CommonModule } from '@angular/common';
 import { NgModule } from '@angular/core';
 import { DropdownModule } from '@sbb-esta/angular-business/dropdown';
-import { IconContextMenuModule } from '@sbb-esta/angular-icons/navigation';
+import { SbbIconModule } from '@sbb-esta/angular-core/icon';
 
 import { ContextmenuComponent } from './contextmenu/contextmenu.component';
 
 @NgModule({
-  imports: [CommonModule, IconContextMenuModule, DropdownModule],
+  imports: [CommonModule, SbbIconModule, DropdownModule],
   declarations: [ContextmenuComponent],
   exports: [ContextmenuComponent, DropdownModule],
 })
