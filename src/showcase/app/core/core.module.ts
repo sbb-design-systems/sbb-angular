d3b9e82bf9fed3bebd75e8ce453881231cffc775
jeripeierSBB <jeremias.peier@sbb.ch>
2020-07-22 11:52:13 +0200

    fix: various fixes (#537)

--- src/showcase/app/core/core.module.ts
+++ src/showcase/app/core/core.module.ts
@@ -18,7 +18,7 @@ import { SharedModule } from '../shared/shared.module';
 import { CoreExamplesModule } from './core-examples/core-examples.module';
 import { CoreRoutingModule } from './core-routing.module';
 import { CoreComponent } from './core/core.component';
-import { CdnIconDialogComponent } from './icon-overview/cdn-icon-list/cdn-icon-detail/cdn-icon-detail.component';
+import { CdnIconDialogComponent } from './icon-overview/cdn-icon-list/cdn-icon-dialog/cdn-icon-dialog.component';
 import { CdnIconListComponent } from './icon-overview/cdn-icon-list/cdn-icon-list.component';
 import { CdnIconComponent } from './icon-overview/cdn-icon-list/cdn-icon/cdn-icon.component';
 import { IconOverviewComponent } from './icon-overview/icon-overview.component';
--- src/showcase/app/core/icon-overview/cdn-icon-list/cdn-icon/cdn-icon.component.ts
+++ src/showcase/app/core/icon-overview/cdn-icon-list/cdn-icon/cdn-icon.component.ts
@@ -2,7 +2,7 @@ import { Component, Input } from '@angular/core';
 import { Dialog } from '@sbb-esta/angular-business/dialog';
 
 import { CdnIcon } from '../../cdn-icon.service';
-import { CdnIconDialogComponent } from '../cdn-icon-detail/cdn-icon-detail.component';
+import { CdnIconDialogComponent } from '../cdn-icon-dialog/cdn-icon-dialog.component';
 
 @Component({
   selector: 'sbb-cdn-icon',
@@ -19,6 +19,9 @@ export class CdnIconComponent {
   constructor(private _dialog: Dialog) {}
 
   openDialog() {
-    this._dialog.openDialog(CdnIconDialogComponent, { data: { cdnIcon: this.cdnIcon } });
+    this._dialog.openDialog(CdnIconDialogComponent, {
+      data: { cdnIcon: this.cdnIcon },
+      minWidth: 420,
+    });
   }
 }
