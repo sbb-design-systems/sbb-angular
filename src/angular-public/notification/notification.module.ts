96bd01a11152d141f77b346d14881bf51f4ac688
jeripeierSBB <jeremias.peier@sbb.ch>
2020-07-23 15:16:48 +0200

    refactor: new icons for notification and 4k styles (#546)

--- src/angular-public/notification/notification.module.ts
+++ src/angular-public/notification/notification.module.ts
@@ -1,22 +1,12 @@
 import { CommonModule } from '@angular/common';
 import { NgModule } from '@angular/core';
+import { SbbIconModule } from '@sbb-esta/angular-core/icon';
 import { IconDirectiveModule } from '@sbb-esta/angular-core/icon-directive';
-import {
-  IconCircleInformationModule,
-  IconSignExclamationPointModule,
-} from '@sbb-esta/angular-icons/basic';
-import { IconTickModule } from '@sbb-esta/angular-icons/status';
 
 import { NotificationComponent } from './notification/notification.component';
 
 @NgModule({
-  imports: [
-    CommonModule,
-    IconTickModule,
-    IconSignExclamationPointModule,
-    IconCircleInformationModule,
-    IconDirectiveModule,
-  ],
+  imports: [CommonModule, SbbIconModule, IconDirectiveModule],
   declarations: [NotificationComponent],
   exports: [NotificationComponent, IconDirectiveModule],
 })
