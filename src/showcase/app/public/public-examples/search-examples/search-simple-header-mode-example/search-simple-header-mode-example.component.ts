5ee041ab2993c7a8717fa6c9c2acec64bd22dd95
jeripeierSBB <jeremias.peier@sbb.ch>
2020-07-24 12:33:21 +0200

    refactor: new icons for search (#550)

---
+++ src/showcase/app/public/public-examples/search-examples/search-simple-header-mode-example/search-simple-header-mode-example.component.ts
@@ -0,0 +1,9 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'sbb-search-simple-header-mode-example',
+  templateUrl: './search-simple-header-mode-example.component.html',
+})
+export class SearchSimpleHeaderModeExample {
+  searchControl: string;
+}
