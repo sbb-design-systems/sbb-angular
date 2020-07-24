98cc2eb99f31ded9374db37eba7661be3b054886
jeripeierSBB <jeremias.peier@sbb.ch>
2020-07-24 09:22:11 +0200

    refactor: new icons for status (#549)

--- src/angular-business/status/status.module.ts
+++ src/angular-business/status/status.module.ts
@@ -1,12 +1,11 @@
 import { CommonModule } from '@angular/common';
 import { NgModule } from '@angular/core';
-import { IconCrossModule } from '@sbb-esta/angular-icons/navigation';
-import { IconExclamationPointModule, IconTickModule } from '@sbb-esta/angular-icons/status';
+import { SbbIconModule } from '@sbb-esta/angular-core/icon';
 
 import { StatusComponent } from './status.component';
 
 @NgModule({
-  imports: [CommonModule, IconCrossModule, IconTickModule, IconExclamationPointModule],
+  imports: [CommonModule, SbbIconModule],
   declarations: [StatusComponent],
   exports: [StatusComponent],
 })
