c1d59cc90d3c51c59c948e03444529bdb6521828
jeripeierSBB <jeremias.peier@sbb.ch>
2020-07-28 08:59:29 +0200

    refactor: new icons for usermenu (#553)

--- src/angular-public/usermenu/usermenu.module.ts
+++ src/angular-public/usermenu/usermenu.module.ts
@@ -1,14 +1,13 @@
 import { CommonModule } from '@angular/common';
 import { NgModule } from '@angular/core';
-import { IconChevronSmallDownModule } from '@sbb-esta/angular-icons/arrow';
-import { IconUserModule } from '@sbb-esta/angular-icons/user';
+import { SbbIconModule } from '@sbb-esta/angular-core/icon';
 import { DropdownModule } from '@sbb-esta/angular-public/dropdown';
 
 import { UserMenuComponent } from './usermenu/usermenu.component';
 
 @NgModule({
   declarations: [UserMenuComponent],
-  imports: [CommonModule, DropdownModule, IconChevronSmallDownModule, IconUserModule],
+  imports: [CommonModule, DropdownModule, SbbIconModule],
   exports: [UserMenuComponent, DropdownModule],
 })
 export class UsermenuModule {}
