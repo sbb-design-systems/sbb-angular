eb1df302461237a4c023470f3f36aac68686cca8
jeripeierSBB <jeremias.peier@sbb.ch>
2020-08-12 16:44:35 +0200

    feat: expand button

--- src/angular-business/sidebar/sidebar-module.ts
+++ src/angular-business/sidebar/sidebar-module.ts
@@ -2,6 +2,7 @@ import { PlatformModule } from '@angular/cdk/platform';
 import { CdkScrollableModule } from '@angular/cdk/scrolling';
 import { CommonModule } from '@angular/common';
 import { NgModule } from '@angular/core';
+import { IconChevronSmallLeftCircleModule } from '@sbb-esta/angular-icons/arrow';
 
 import {
   SbbIconSidebar,
@@ -12,7 +13,7 @@ import { SbbIconSidebarItem } from './icon-sidebar/icon-sidebar-item';
 import { SbbSidebar, SbbSidebarContainer, SbbSidebarContent } from './sidebar/sidebar';
 
 @NgModule({
-  imports: [CommonModule, PlatformModule, CdkScrollableModule],
+  imports: [CommonModule, PlatformModule, CdkScrollableModule, IconChevronSmallLeftCircleModule],
   exports: [
     CdkScrollableModule,
 
