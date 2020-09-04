ecd26626de150820b4397da0ca87afd2a8ae1b88
jeripeierSBB <jeremias.peier@sbb.ch>
2020-09-04 12:27:21 +0200

    refactor: add sidebar to public api

--- src/angular-business/public-api.ts
+++ src/angular-business/public-api.ts
@@ -24,6 +24,7 @@ export * from '@sbb-esta/angular-business/pagination';
 export * from '@sbb-esta/angular-business/processflow';
 export * from '@sbb-esta/angular-business/radio-button';
 export * from '@sbb-esta/angular-business/select';
+export * from '@sbb-esta/angular-business/sidebar';
 export * from '@sbb-esta/angular-business/status';
 export * from '@sbb-esta/angular-business/table';
 export * from '@sbb-esta/angular-business/tabs';
