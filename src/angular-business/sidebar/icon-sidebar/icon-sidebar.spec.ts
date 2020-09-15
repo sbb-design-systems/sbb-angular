44e185d70592f6f49bf93243317a3425e38b02b1
jeripeierSBB <jeremias.peier@sbb.ch>
2020-09-15 14:27:01 +0200

    refactor: move fake mediaMatcher to testing module
    Signed-off-by: jeripeierSBB <jeremias.peier@sbb.ch>

--- src/angular-business/sidebar/icon-sidebar/icon-sidebar.spec.ts
+++ src/angular-business/sidebar/icon-sidebar/icon-sidebar.spec.ts
@@ -3,7 +3,7 @@ import { MediaMatcher } from '@angular/cdk/layout';
 import { PlatformModule } from '@angular/cdk/platform';
 import { CdkScrollable } from '@angular/cdk/scrolling';
 import { CommonModule } from '@angular/common';
-import { Component, DebugElement, ElementRef, Injectable, ViewChild } from '@angular/core';
+import { Component, DebugElement, ElementRef, ViewChild } from '@angular/core';
 import { async, ComponentFixture, fakeAsync, inject, TestBed, tick } from '@angular/core/testing';
 import { By } from '@angular/platform-browser';
 import {
@@ -14,6 +14,7 @@ import { RouterTestingModule } from '@angular/router/testing';
 import { Breakpoints } from '@sbb-esta/angular-core/breakpoints';
 import { SbbIconModule } from '@sbb-esta/angular-core/icon';
 import { SbbIconTestingModule } from '@sbb-esta/angular-core/icon/testing';
+import { FakeMediaMatcher } from '@sbb-esta/angular-core/testing';
 
 import { SbbSidebarModule } from '../sidebar.module';
 
@@ -61,6 +62,7 @@ describe('SbbIconSidebar', () => {
 
   beforeEach(inject([MediaMatcher], (mm: FakeMediaMatcher) => {
     mediaMatcher = mm;
+    mediaMatcher.defaultMatches = false;
   }));
 
   afterEach(() => {
@@ -544,69 +546,6 @@ class IconSidebarWithLinksTestComponent {
       this.expandedCount++;
     } else {
       this.collapsedCount++;
-    }
-  }
-}
-
-export class FakeMediaQueryList {
-  /** The callback for change events. */
-  private _listeners: ((mql: MediaQueryListEvent) => void)[] = [];
-
-  constructor(public matches: boolean, public media: string) {}
-
-  /** Toggles the matches state and "emits" a change event. */
-  setMatches(matches: boolean) {
-    this.matches = matches;
-
-    /** Simulate an asynchronous task. */
-    setTimeout(() => {
-      this._listeners.forEach((listener) => listener(this as any));
-    });
-  }
-
-  /** Registers a callback method for change events. */
-  addListener(callback: (mql: MediaQueryListEvent) => void) {
-    this._listeners.push(callback);
-  }
-
-  /** Removes a callback method from the change events. */
-  removeListener(callback: (mql: MediaQueryListEvent) => void) {
-    const index = this._listeners.indexOf(callback);
-
-    if (index > -1) {
-      this._listeners.splice(index, 1);
-    }
-  }
-}
-
-@Injectable()
-export class FakeMediaMatcher {
-  /** A map of match media queries. */
-  private _queries = new Map<string, FakeMediaQueryList>();
-
-  /** The number of distinct queries created in the media matcher during a test. */
-  get queryCount(): number {
-    return this._queries.size;
-  }
-
-  /** Fakes the match media response to be controlled in tests. */
-  matchMedia(query: string): FakeMediaQueryList {
-    const mql = new FakeMediaQueryList(false, query);
-    this._queries.set(query, mql);
-    return mql;
-  }
-
-  /** Clears all queries from the map of queries. */
-  clear() {
-    this._queries.clear();
-  }
-
-  /** Toggles the matching state of the provided query. */
-  setMatchesQuery(query: string, matches: boolean) {
-    if (this._queries.has(query)) {
-      this._queries.get(query)!.setMatches(matches);
-    } else {
-      throw Error('This query is not being observed.');
     }
   }
 }
