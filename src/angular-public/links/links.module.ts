d86dcb6c03495a8f5bbe665b22e163e2e9eb0ad9
Lukas Spirig <lukas.spirig@sbb.ch>
2020-10-02 13:47:42 +0200

    refactor: remove social link
    BREAKING CHANGE: We are removing the social link, since the icon CDN does not
    contain the social icons and the social link had no usage.

--- src/angular-public/links/links.module.ts
+++ src/angular-public/links/links.module.ts
@@ -1,33 +1,13 @@
 import { CommonModule } from '@angular/common';
 import { NgModule } from '@angular/core';
 import { SbbIconModule, ɵSBB_ICON_REGISTRY_WRAPPER_PROVIDER } from '@sbb-esta/angular-core/icon';
-import {
-  IconFacebookModule,
-  IconInstagramModule,
-  IconLinkedinModule,
-  IconPinterestModule,
-  IconTwitterModule,
-  IconXingModule,
-  IconYoutubeModule,
-} from '@sbb-esta/angular-icons/social-media';
 
 import { SbbLink } from './link/link.component';
-import { SbbSocialLink } from './social-link/social-link.component';
 
 @NgModule({
-  imports: [
-    CommonModule,
-    SbbIconModule,
-    IconFacebookModule,
-    IconInstagramModule,
-    IconPinterestModule,
-    IconTwitterModule,
-    IconYoutubeModule,
-    IconXingModule,
-    IconLinkedinModule,
-  ],
-  declarations: [SbbLink, SbbSocialLink],
-  exports: [SbbLink, SbbSocialLink],
+  imports: [CommonModule, SbbIconModule],
+  declarations: [SbbLink],
+  exports: [SbbLink],
   providers: [ɵSBB_ICON_REGISTRY_WRAPPER_PROVIDER],
 })
 export class SbbLinksModule {}
