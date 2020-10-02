8f098228c18d86e1b1029d717e8dea2ac505ecfa
Lukas Spirig <lukas.spirig@sbb.ch>
2020-10-02 16:57:34 +0200

    fix: import path

--- src/angular-business/links/link.component.spec.ts
+++ src/angular-business/links/link.component.spec.ts
@@ -3,7 +3,7 @@ import { async, ComponentFixture, TestBed } from '@angular/core/testing';
 import { By } from '@angular/platform-browser';
 import { SbbIconTestingModule } from '@sbb-esta/angular-core/icon/testing';
 
-import { SbbLinksModule } from '../links.module';
+import { SbbLinksModule } from './links.module';
 
 @Component({
   selector: 'sbb-test-link',
