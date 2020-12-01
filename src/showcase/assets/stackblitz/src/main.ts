2e31ad0d50166ee38a4297e246220976dcba615e
Lukas Spirig <lukas.spirig@sbb.ch>
2020-12-01 16:58:30 +0100

    fix(showcase): use specific version for StackBlitz dependencies

--- src/showcase/assets/stackblitz/src/main.ts
+++ src/showcase/assets/stackblitz/src/main.ts
@@ -1,17 +1,8 @@
+import './polyfills';
+
 import { platformBrowserDynamic } from '@angular/platform-browser-dynamic';
 
 import { AppModule } from './app/app.module';
-import './polyfills';
 
-platformBrowserDynamic()
-  .bootstrapModule(AppModule)
-  .then((ref) => {
-    // Ensure Angular destroys itself on hot reloads.
-    if (window['ngRef']) {
-      window['ngRef'].destroy();
-    }
-    window['ngRef'] = ref;
-
-    // Otherwise, log the boot error
-  })
+platformBrowserDynamic().bootstrapModule(AppModule)
   .catch((err) => console.error(err));
