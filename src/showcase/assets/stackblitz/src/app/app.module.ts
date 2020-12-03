a2f5d128b772819c12105d9fac2dcf5f99b114ed
Lukas Spirig <lukas.spirig@sbb.ch>
2020-12-03 17:08:46 +0100

    fix(showcase): fix StackBlitz setup

--- src/showcase/assets/stackblitz/src/app/app.module.ts
+++ src/showcase/assets/stackblitz/src/app/app.module.ts
@@ -1,3 +1,4 @@
+import { HttpClientModule } from '@angular/common/http';
 import { NgModule } from '@angular/core';
 import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 import { BrowserModule } from '@angular/platform-browser';
@@ -7,7 +8,7 @@ import { SbbModule } from './{moduleName}';
 import { {componentName} } from './{selectorName}';
 
 @NgModule({
-  imports:      [ BrowserModule, BrowserAnimationsModule, FormsModule, ReactiveFormsModule, SbbModule ],
+  imports:      [ BrowserModule, BrowserAnimationsModule, HttpClientModule, FormsModule, ReactiveFormsModule, SbbModule ],
   declarations: [ {componentName} ],
   bootstrap:    [ {componentName} ],
 })
