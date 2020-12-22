855b416faeb916968ff4acfcc3a17d343181a543
jeripeierSBB <jeremias.peier@sbb.ch>
2020-12-22 17:19:43 +0100

    fix(showcase): remove obsolete code in sbb-expansion-panel example (#696)

--- src/showcase/app/public/public-examples/accordion-examples/simple-panel-example/simple-panel-example.component.ts
+++ src/showcase/app/public/public-examples/accordion-examples/simple-panel-example/simple-panel-example.component.ts
@@ -1,29 +1,7 @@
 import { Component } from '@angular/core';
-import { FormControl, FormGroup } from '@angular/forms';
 
 @Component({
   selector: 'sbb-simple-panel-example',
   templateUrl: './simple-panel-example.component.html',
 })
-export class SimplePanelExampleComponent {
-  panelMode = 'panel 1';
-  panels = ['panel 1', 'panel 2', 'panel 3', 'panel 4', 'panel 5'];
-  accordionForm: FormGroup;
-
-  radioOptions = [
-    {
-      name: 'Open all',
-      value: 'openAll',
-    },
-    {
-      name: 'Close all',
-      value: 'closeAll',
-    },
-  ];
-
-  constructor() {
-    this.accordionForm = new FormGroup({
-      panelMode: new FormControl(this.panelMode),
-    });
-  }
-}
+export class SimplePanelExampleComponent {}
