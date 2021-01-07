e7c0c1fdbf71e4348186dd119d92314d5e6f1a00
jeripeierSBB <jeremias.peier@sbb.ch>
2021-01-07 11:25:55 +0100

    feat: merge symbols find nearest bazel file

--- schematics/merge-symbols/index.js
+++ schematics/merge-symbols/index.js
@@ -4,6 +4,7 @@ Object.defineProperty(exports, '__esModule', { value: true });
 
 function _interopDefault (ex) { return (ex && (typeof ex === 'object') && 'default' in ex) ? ex['default'] : ex; }
 
+var core = require('@angular-devkit/core');
 var ts = _interopDefault(require('@schematics/angular/third_party/github.com/Microsoft/TypeScript/lib/typescript'));
 
 const IGNORED_FOLDERS = [
@@ -13,46 +14,84 @@ const IGNORED_FOLDERS = [
 function mergeSymbols() {
     return (tree) => {
         const symbols = {};
-        tree.getDir(`src/angular`).visit((filePath, moduleDirEntry) => {
-            if (!(moduleDirEntry && filePath.endsWith('.ts') && !filePath.endsWith('.spec.ts')) ||
-                isInIgnoredFolders(filePath)) {
-                return;
-            }
-            const tsFile = ts.createSourceFile(filePath, moduleDirEntry.content.toString(), ts.ScriptTarget.Latest);
-            tsFile.statements.filter(hasExportModifier).forEach((statement) => {
-                const modulePath = filePath; // TODO: resolve filePath
-                if (ts.isVariableStatement(statement)) {
-                    statement.declarationList.declarations
-                        .map((declaration) => declaration.name)
-                        .filter(ts.isIdentifier)
-                        .map((identifier) => identifier.escapedText)
-                        .forEach((name) => addToSymbols(name, modulePath));
-                }
-                else if (statement.name &&
-                    statement.name.kind === ts.SyntaxKind.Identifier) {
-                    const typedName = statement.name;
-                    addToSymbols(typedName.escapedText, modulePath);
-                }
-            });
-        });
+        extractExportsForModule('src/angular/');
+        extractExportsForModule('src/angular-maps/');
         // TODO: sort by module path and then by key
         const sortedSymbols = Object.keys(symbols)
             .sort()
             .reduce((r, k) => ((r[k] = symbols[k]), r), {});
         // TODO: write to filesystem
-        console.log(sortedSymbols);
+        console.log(JSON.stringify(sortedSymbols, null, 4));
+        function extractExportsForModule(rootPath) {
+            tree.getDir(rootPath).visit((filePath, moduleDirEntry) => {
+                if (!(moduleDirEntry && filePath.endsWith('.ts') && !filePath.endsWith('.spec.ts')) ||
+                    isInIgnoredFolders(filePath)) {
+                    return;
+                }
+                const tsFile = ts.createSourceFile(filePath, moduleDirEntry.content.toString(), ts.ScriptTarget.Latest);
+                tsFile.statements.filter(hasExportModifier).forEach((statement) => {
+                    extractSymbolOfStatement(filePath, rootPath, statement);
+                });
+            });
+        }
+        function isInIgnoredFolders(filePath) {
+            return IGNORED_FOLDERS.some((folder) => filePath.startsWith(folder));
+        }
         function hasExportModifier(statement) {
             return (statement.modifiers &&
                 statement.modifiers.some((modifier) => modifier.kind === ts.SyntaxKind.ExportKeyword));
+        }
+        function extractSymbolOfStatement(filePath, rootPath, statement) {
+            const modulePath = resolvePackageModule(filePath).replace('/' + rootPath, '');
+            if (ts.isVariableStatement(statement)) {
+                statement.declarationList.declarations
+                    .map((declaration) => declaration.name)
+                    .filter(ts.isIdentifier)
+                    .map((identifier) => identifier.escapedText)
+                    .forEach((name) => addToSymbols(name, modulePath));
+            }
+            else if (statement.name &&
+                statement.name.kind === ts.SyntaxKind.Identifier) {
+                const typedName = statement.name;
+                addToSymbols(typedName.escapedText, modulePath);
+            }
+        }
+        function resolvePackageModule(path) {
+            let dir;
+            if (!isDir(path)) {
+                dir = parentDir(path);
+            }
+            else {
+                dir = path;
+            }
+            const bazelFile = tree.getDir(dir).file('BUILD.bazel');
+            if (bazelFile == null) {
+                return resolvePackageModule(parentDir(dir));
+            }
+            return dir;
+        }
+        function isDir(path) {
+            try {
+                tree.getDir(path);
+            }
+            catch (e) {
+                if (e instanceof core.PathIsFileException) {
+                    return false;
+                }
+                else {
+                    throw Error;
+                }
+            }
+            return true;
+        }
+        function parentDir(path) {
+            return path.split('/').slice(0, -1).join('/');
         }
         function addToSymbols(name, modulePath) {
             if (symbols[name] && symbols[name] !== modulePath) {
                 console.warn(`symbol ${name} is already in list with value ${symbols[name]}. Tried to add ${modulePath}.`);
             }
             symbols[name] = modulePath;
-        }
-        function isInIgnoredFolders(filePath) {
-            return IGNORED_FOLDERS.some((folder) => filePath.startsWith(folder));
         }
     };
 }
--- schematics/merge-symbols/index.ts
+++ schematics/merge-symbols/index.ts
@@ -1,4 +1,4 @@
-import { Path } from '@angular-devkit/core';
+import { Path, PathFragment, PathIsFileException } from '@angular-devkit/core';
 import { Rule, Tree } from '@angular-devkit/schematics';
 import ts from '@schematics/angular/third_party/github.com/Microsoft/TypeScript/lib/typescript';
 
@@ -11,38 +11,8 @@ export function mergeSymbols(): Rule {
   return (tree: Tree) => {
     const symbols: { [key: string]: string } = {};
 
-    tree.getDir(`src/angular`).visit((filePath, moduleDirEntry) => {
-      if (
-        !(moduleDirEntry && filePath.endsWith('.ts') && !filePath.endsWith('.spec.ts')) ||
-        isInIgnoredFolders(filePath)
-      ) {
-        return;
-      }
-
-      const tsFile = ts.createSourceFile(
-        filePath,
-        moduleDirEntry.content.toString(),
-        ts.ScriptTarget.Latest
-      );
-
-      tsFile.statements.filter(hasExportModifier).forEach((statement) => {
-        const modulePath = filePath; // TODO: resolve filePath
-
-        if (ts.isVariableStatement(statement)) {
-          statement.declarationList.declarations
-            .map((declaration) => declaration.name)
-            .filter(ts.isIdentifier)
-            .map((identifier) => identifier.escapedText)
-            .forEach((name) => addToSymbols(name as string, modulePath));
-        } else if (
-          (statement as ts.DeclarationStatement).name &&
-          (statement as ts.DeclarationStatement).name!.kind === ts.SyntaxKind.Identifier
-        ) {
-          const typedName = (statement as ts.DeclarationStatement).name! as ts.Identifier;
-          addToSymbols(typedName.escapedText as string, modulePath);
-        }
-      });
-    });
+    extractExportsForModule('src/angular/');
+    extractExportsForModule('src/angular-maps/');
 
     // TODO: sort by module path and then by key
     const sortedSymbols = Object.keys(symbols)
@@ -50,13 +20,90 @@ export function mergeSymbols(): Rule {
       .reduce((r: { [key: string]: string }, k) => ((r[k] = symbols[k]), r), {});
 
     // TODO: write to filesystem
-    console.log(sortedSymbols);
+    console.log(JSON.stringify(sortedSymbols, null, 4));
+
+    function extractExportsForModule(rootPath: string) {
+      tree.getDir(rootPath).visit((filePath, moduleDirEntry) => {
+        if (
+          !(moduleDirEntry && filePath.endsWith('.ts') && !filePath.endsWith('.spec.ts')) ||
+          isInIgnoredFolders(filePath)
+        ) {
+          return;
+        }
+
+        const tsFile = ts.createSourceFile(
+          filePath,
+          moduleDirEntry.content.toString(),
+          ts.ScriptTarget.Latest
+        );
+
+        tsFile.statements.filter(hasExportModifier).forEach((statement) => {
+          extractSymbolOfStatement(filePath, rootPath, statement);
+        });
+      });
+    }
+
+    function isInIgnoredFolders(filePath: Path) {
+      return IGNORED_FOLDERS.some((folder) => filePath.startsWith(folder));
+    }
 
     function hasExportModifier(statement: ts.Statement) {
       return (
         statement.modifiers &&
         statement.modifiers.some((modifier) => modifier.kind === ts.SyntaxKind.ExportKeyword)
       );
+    }
+
+    function extractSymbolOfStatement(filePath: Path, rootPath: string, statement: ts.Statement) {
+      const modulePath = resolvePackageModule(filePath).replace('/' + rootPath, '');
+
+      if (ts.isVariableStatement(statement)) {
+        statement.declarationList.declarations
+          .map((declaration) => declaration.name)
+          .filter(ts.isIdentifier)
+          .map((identifier) => identifier.escapedText)
+          .forEach((name) => addToSymbols(name as string, modulePath));
+      } else if (
+        (statement as ts.DeclarationStatement).name &&
+        (statement as ts.DeclarationStatement).name!.kind === ts.SyntaxKind.Identifier
+      ) {
+        const typedName = (statement as ts.DeclarationStatement).name! as ts.Identifier;
+        addToSymbols(typedName.escapedText as string, modulePath);
+      }
+    }
+
+    function resolvePackageModule(path: string): string {
+      let dir;
+
+      if (!isDir(path)) {
+        dir = parentDir(path);
+      } else {
+        dir = path;
+      }
+
+      const bazelFile = tree.getDir(dir).file('BUILD.bazel' as PathFragment);
+      if (bazelFile == null) {
+        return resolvePackageModule(parentDir(dir));
+      }
+
+      return dir;
+    }
+
+    function isDir(path: string) {
+      try {
+        tree.getDir(path);
+      } catch (e) {
+        if (e instanceof PathIsFileException) {
+          return false;
+        } else {
+          throw Error;
+        }
+      }
+      return true;
+    }
+
+    function parentDir(path: string) {
+      return path.split('/').slice(0, -1).join('/');
     }
 
     function addToSymbols(name: string, modulePath: string) {
@@ -66,10 +113,6 @@ export function mergeSymbols(): Rule {
         );
       }
       symbols[name] = modulePath;
-    }
-
-    function isInIgnoredFolders(filePath: Path) {
-      return IGNORED_FOLDERS.some((folder) => filePath.startsWith(folder));
     }
   };
 }
