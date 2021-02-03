e3e2dc4a2637f8af4de79184c5e8d5ccdb91a918
Lukas Spirig <lukas.spirig@sbb.ch>
2021-02-03 12:34:47 +0100

    fix: revert group classes and genrate symbols

--- src/angular/schematics/ng-add/data/css-selectors.ts
+++ src/angular/schematics/ng-add/data/css-selectors.ts
@@ -1,4 +1,4 @@
-import { TargetVersion, VersionChanges } from '@angular/cdk/schematics';
+import { VersionChanges } from '@angular/cdk/schematics';
 
 export interface SbbAngularCssSelectorData {
   /** The CSS selector to replace. */
@@ -19,28 +19,4 @@ export interface SbbAngularCssSelectorData {
   };
 }
 
-export const cssSelectors: VersionChanges<SbbAngularCssSelectorData> = {
-  ['merge' as TargetVersion]: [
-    {
-      pr: '',
-      changes: [
-        {
-          replace: 'sbb-checkbox-group-vertical',
-          replaceWith: 'sbb-selection-group-vertical',
-        },
-        {
-          replace: 'sbb-radio-group-vertical',
-          replaceWith: 'sbb-selection-group-vertical',
-        },
-        {
-          replace: 'sbb-checkbox-group-horizontal',
-          replaceWith: 'sbb-selection-group-horizontal',
-        },
-        {
-          replace: 'sbb-radio-group-horizontal',
-          replaceWith: 'sbb-selection-group-horizontal',
-        },
-      ],
-    },
-  ],
-};
+export const cssSelectors: VersionChanges<SbbAngularCssSelectorData> = {};
