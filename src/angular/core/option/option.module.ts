44caebb8f122e7ffe521609bf86929b31fb3fd3e
Lukas Spirig <lukas.spirig@sbb.ch>
2021-02-08 10:01:33 +0100

    refactor: pseudo-checkbox

--- src/angular/core/option/option.module.ts
+++ src/angular/core/option/option.module.ts
@@ -1,15 +1,13 @@
 import { CommonModule } from '@angular/common';
 import { NgModule } from '@angular/core';
-import { SbbIconModule, ɵSBB_ICON_REGISTRY_WRAPPER_PROVIDER } from '@sbb-esta/angular/icon';
 
 import { SbbOptgroup } from './option-group/option-group.component';
 import { SbbOption } from './option/option.component';
 import { SbbPseudoCheckbox } from './option/pseudo-checkbox';
 
 @NgModule({
-  imports: [CommonModule, SbbIconModule],
+  imports: [CommonModule],
   declarations: [SbbOption, SbbOptgroup, SbbPseudoCheckbox],
   exports: [SbbOption, SbbOptgroup, SbbPseudoCheckbox],
-  providers: [ɵSBB_ICON_REGISTRY_WRAPPER_PROVIDER],
 })
 export class SbbOptionModule {}
--- src/angular/core/option/option/pseudo-checkbox.ts
+++ src/angular/core/option/option/pseudo-checkbox.ts
@@ -5,15 +5,16 @@ import { ChangeDetectionStrategy, Component, Input, ViewEncapsulation } from '@a
   templateUrl: './pseudo-checkbox.html',
   host: {
     class: 'sbb-pseudo-checkbox',
-    '[class.sbb-pseudo-checkbox-disabled]': 'this.disabled',
-    '[class.sbb-pseudo-checkbox-checked]': 'this.state === "checked"',
+    '[class.sbb-selection-disabled]': 'this.disabled',
+    '[class.sbb-selection-checked]': 'this.state === "checked"',
+    '[class.sbb-selection-indeterminate]': 'this.state === "indeterminate"',
   },
   encapsulation: ViewEncapsulation.None,
   changeDetection: ChangeDetectionStrategy.OnPush,
 })
 export class SbbPseudoCheckbox {
   /** Display state of the checkbox. */
-  @Input() state: 'unchecked' | 'checked' = 'unchecked';
+  @Input() state: 'unchecked' | 'checked' | 'indeterminate' = 'unchecked';
 
   /** Whether the checkbox is disabled. */
   @Input() disabled: boolean = false;
