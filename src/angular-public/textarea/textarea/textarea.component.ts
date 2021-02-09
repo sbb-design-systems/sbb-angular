76e4438d17212553faeab1be98c3a2aa2d26534d
e503423 <aron.schneider@sbb.ch>
2021-02-09 09:53:56 +0100

    fix: fixed readonly styling in textarea

--- src/angular-public/textarea/textarea/textarea.component.ts
+++ src/angular-public/textarea/textarea/textarea.component.ts
@@ -65,6 +65,7 @@ export const SbbTextareaMixinBase: CanUpdateErrorStateCtor &
     '[attr.aria-describedby]': '_ariaDescribedby || null',
     '[class.sbb-disabled]': 'disabled',
     '[class.sbb-focused]': 'focused',
+    '[class.sbb-readonly]': 'readonly',
   },
 })
 export class SbbTextarea
