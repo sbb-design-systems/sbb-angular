d960dbe592e330685c0b2fc9758d399eb9703ab5
jeripeierSBB <jeremias.peier@sbb.ch>
2021-02-23 17:15:49 +0100

    refactor: update option

--- src/angular-public/option/public-api.ts
+++ src/angular-public/option/public-api.ts
@@ -2,6 +2,7 @@ export * from './option.module';
 export * from './option/option.component';
 export * from './option-group/option-group.component';
 export * from './option/pseudo-checkbox';
+export * from './option-parent';
 /** @deprecated Remove with v12 */
 export { SbbOptionModule as OptionModule } from './option.module';
 /** @deprecated Remove with v12 */
