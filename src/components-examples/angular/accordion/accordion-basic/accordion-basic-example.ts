172a457f009e46e5291bc90f40c1cb22db3a8ca3
jeripeierSBB <jeremias.peier@sbb.ch>
2021-03-17 11:54:17 +0100

    refactor: basic example

--- src/components-examples/angular/accordion/accordion-basic/accordion-basic-example.ts
+++ src/components-examples/angular/accordion/accordion-basic/accordion-basic-example.ts
@@ -12,14 +12,11 @@ import { SbbAccordion } from '@sbb-esta/angular/accordion';
   styleUrls: ['./accordion-basic-example.css'],
 })
 export class AccordionBasicExample {
-  @ViewChild(SbbAccordion, { static: true }) firstAccordion: SbbAccordion;
-
-  disabled = new FormControl(false);
-  panelMode = new FormControl('panel 1');
+  multi = new FormControl(false);
   hideToggle = new FormControl(false);
-  panelOpenState = false;
-  panels = ['panel 1', 'panel 2', 'panel 3', 'panel 4', 'panel 5'];
-  multi = false;
+  disabled = new FormControl(false);
+  firstPanelExpanded = false;
+  secondPanelExpanded = false;
 
   log(...args: any[]) {
     console.log(args);
