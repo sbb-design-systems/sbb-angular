5bc23d58a034009ae6234d207fa732e750b6f82c
jeripeierSBB <jeremias.peier@sbb.ch>
2021-04-08 16:17:42 +0200

    refactor: rename icon module

--- src/angular/icon/public-api.ts
+++ src/angular/icon/public-api.ts
@@ -1,5 +1,5 @@
 export { SBB_ICON_REGISTRY_WRAPPER_PROVIDER as ɵSBB_ICON_REGISTRY_WRAPPER_PROVIDER } from './icon-registry-wrapper-provider';
-export * from './icon-module';
+export * from './icon.module';
 export * from './icon';
 export * from './icon-registry';
 export * from './icon-cdn-provider';
