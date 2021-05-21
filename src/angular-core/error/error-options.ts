0e8526e3517de85c098fc9a0e2f76988e456dadc
jeripeierSBB <jeremias.peier@sbb.ch>
2021-05-21 10:57:34 +0200

    refactor: relax SbbErrorStateMatcher from FormControl to AbstractControl

--- src/angular-core/error/error-options.ts
+++ src/angular-core/error/error-options.ts
@@ -1,10 +1,10 @@
 import { Injectable } from '@angular/core';
-import { FormControl, FormGroupDirective, NgForm } from '@angular/forms';
+import { AbstractControl, FormGroupDirective, NgForm } from '@angular/forms';
 
 /** Provider that defines how form controls behave with regards to displaying error messages. */
 @Injectable({ providedIn: 'root' })
 export class SbbErrorStateMatcher {
-  isErrorState(control: FormControl | null, form: FormGroupDirective | NgForm | null): boolean {
+  isErrorState(control: AbstractControl | null, form: FormGroupDirective | NgForm | null): boolean {
     return !!(control && control.invalid && (control.touched || (form && form.submitted)));
   }
 }
@@ -12,7 +12,7 @@ export class SbbErrorStateMatcher {
 /** Error state matcher that matches when a control is invalid and dirty. */
 @Injectable()
 export class SbbShowOnDirtyErrorStateMatcher implements SbbErrorStateMatcher {
-  isErrorState(control: FormControl | null, form: FormGroupDirective | NgForm | null): boolean {
+  isErrorState(control: AbstractControl | null, form: FormGroupDirective | NgForm | null): boolean {
     return !!(control && control.invalid && (control.dirty || (form && form.submitted)));
   }
 }
