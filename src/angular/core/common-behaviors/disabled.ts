7d446409b734883a74f0f93b412ba135ec41f14f
jeripeierSBB <jeremias.peier@sbb.ch>
2021-06-08 11:43:38 +0200

    refactor: compare common-behaviors to material

--- src/angular/core/common-behaviors/disabled.ts
+++ src/angular/core/common-behaviors/disabled.ts
@@ -1,6 +1,6 @@
 import { coerceBooleanProperty } from '@angular/cdk/coercion';
 
-import { Constructor } from './constructor';
+import { AbstractConstructor, Constructor } from './constructor';
 
 /** @docs-private */
 export interface CanDisable {
@@ -8,13 +8,18 @@ export interface CanDisable {
   disabled: boolean;
 }
 
-/** @docs-private */
-export type CanDisableCtor = Constructor<CanDisable>;
+/**
+ * @docs-private
+ * @deprecated No longer necessary to apply to mixin classes. To be made private.
+ * @breaking-change 13.0.0
+ */
+export type CanDisableCtor = Constructor<CanDisable> & AbstractConstructor<CanDisable>;
 
 /** Mixin to augment a directive with a `disabled` property. */
+export function mixinDisabled<T extends AbstractConstructor<{}>>(base: T): CanDisableCtor & T;
 export function mixinDisabled<T extends Constructor<{}>>(base: T): CanDisableCtor & T {
   return class extends base {
-    private _disabled = false;
+    private _disabled: boolean = false;
 
     get disabled() {
       return this._disabled;
--- src/angular/core/common-behaviors/error-state.ts
+++ src/angular/core/common-behaviors/error-state.ts
@@ -3,7 +3,7 @@ import { Subject } from 'rxjs';
 
 import { SbbErrorStateMatcher } from '../error/error-options';
 
-import { Constructor } from './constructor';
+import { AbstractConstructor, Constructor } from './constructor';
 
 /** @docs-private */
 export interface CanUpdateErrorState {
@@ -13,8 +13,13 @@ export interface CanUpdateErrorState {
   updateErrorState(): void;
 }
 
-/** @docs-private */
-export type CanUpdateErrorStateCtor = Constructor<CanUpdateErrorState>;
+/**
+ * @docs-private
+ * @deprecated No longer necessary to apply to mixin classes. To be made private.
+ * @breaking-change 13.0.0
+ */
+export type CanUpdateErrorStateCtor = Constructor<CanUpdateErrorState> &
+  AbstractConstructor<CanUpdateErrorState>;
 
 /** @docs-private */
 export interface HasErrorState {
@@ -28,12 +33,15 @@ export interface HasErrorState {
  * Mixin to augment a directive with updateErrorState method.
  * For component with `errorState` and need to update `errorState`.
  */
+export function mixinErrorState<T extends AbstractConstructor<HasErrorState>>(
+  base: T
+): CanUpdateErrorStateCtor & T;
 export function mixinErrorState<T extends Constructor<HasErrorState>>(
   base: T
 ): CanUpdateErrorStateCtor & T {
   return class extends base {
     /** Whether the component is in an error state. */
-    errorState = false;
+    errorState: boolean = false;
 
     /**
      * Stream that emits whenever the state of the input changes such that the wrapping
