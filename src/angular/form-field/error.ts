6c198d989f0a6b20201c6c4105c995782ef84c53
jeripeierSBB <jeremias.peier@sbb.ch>
2021-06-08 15:25:38 +0200

    refactor: update form-field to material

--- src/angular/form-field/error.ts
+++ src/angular/form-field/error.ts
@@ -1,4 +1,4 @@
-import { Directive, InjectionToken, Input } from '@angular/core';
+import { Attribute, Directive, ElementRef, InjectionToken, Input } from '@angular/core';
 
 let nextId = 0;
 
@@ -14,11 +14,19 @@ export const SBB_ERROR = new InjectionToken<SbbError>('SbbError');
   selector: 'sbb-error, [sbbError]',
   host: {
     class: 'sbb-error',
-    role: 'alert',
     '[attr.id]': 'id',
+    'aria-atomic': 'true',
   },
   providers: [{ provide: SBB_ERROR, useExisting: SbbError }],
 })
 export class SbbError {
   @Input() id: string = `sbb-error-${nextId++}`;
+
+  constructor(@Attribute('aria-live') ariaLive: string, elementRef: ElementRef) {
+    // If no aria-live value is set add 'polite' as a default. This is preferred over setting
+    // role='alert' so that screen readers do not interrupt the current task to read this aloud.
+    if (!ariaLive) {
+      elementRef.nativeElement.setAttribute('aria-live', 'polite');
+    }
+  }
 }
