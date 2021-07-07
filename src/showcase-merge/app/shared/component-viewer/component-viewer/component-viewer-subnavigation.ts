100c71b325325ec58031c1c85407e6eb71764bd4
jeripeierSBB <jeremias.peier@sbb.ch>
2021-07-07 16:19:10 +0200

    refactor: review jeri

--- src/showcase-merge/app/shared/component-viewer/component-viewer/component-viewer-subnavigation.ts
+++ src/showcase-merge/app/shared/component-viewer/component-viewer/component-viewer-subnavigation.ts
@@ -1,8 +1,10 @@
+import { Routes } from '@angular/router';
+
 import { LoaderBuilder } from '../../loader-builder';
 import { ExampleListViewerComponent } from '../example-list-viewer/example-list-viewer.component';
 import { HtmlViewerComponent } from '../html-viewer/html-viewer.component';
 
-export const componentViewerSubnavigation = [
+export const componentViewerSubnavigation: Routes = [
   {
     path: 'overview',
     component: HtmlViewerComponent,
