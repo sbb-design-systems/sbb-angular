9f8dd4a751ea8e75e2bb2eea68007972cc098e8f
Lukas Spirig <lukas.spirig@sbb.ch>
2021-07-27 09:20:29 +0200

    feat: add css class for normal error flow inside sbb-form-field (#853)
    We are adding the css class `sbb-form-field-flexible-errors`, which
    can be placed on `sbb-form-field` or any ancestor, which will remove
    the reserved space for errors and display errors in the normal
    context flow.
    This also deprecates the css class `sbb-form-field-errorless`, which
    will be removed in a future version.

--- src/angular/schematics/ng-add/test-cases/merge/form-field_input.ts
+++ src/angular/schematics/ng-add/test-cases/merge/form-field_input.ts
@@ -1,2 +1,13 @@
+import { Component } from '@angular/core';
 import { SbbFormFieldModule } from '@sbb-esta/angular-public/form-field';
 import { SbbFormFieldControl } from '@sbb-esta/angular-core/forms';
+
+@Component({
+  selector: 'test',
+  template: `
+    <sbb-form-field label="Test" class="sbb-form-field-errorless">
+      <input sbbInput>
+    </sbb-form-field>
+  `,
+})
+export class TestComponent {}
