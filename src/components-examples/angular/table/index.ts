b47ee476c5a3b64b610e32fda68ec1af1912d709
jeripeierSBB <jeremias.peier@sbb.ch>
2021-08-11 18:48:12 +0200

    fix: examples

--- src/components-examples/angular/table/index.ts
+++ src/components-examples/angular/table/index.ts
@@ -1,17 +1,45 @@
 import { CommonModule } from '@angular/common';
 import { NgModule } from '@angular/core';
 import { FormsModule, ReactiveFormsModule } from '@angular/forms';
+import { SbbAutocompleteModule } from '@sbb-esta/angular/autocomplete';
 import { SbbButtonModule } from '@sbb-esta/angular/button';
 import { SbbCheckboxModule } from '@sbb-esta/angular/checkbox';
-import { SbbFormFieldModule } from '@sbb-esta/angular/form-field';
+import { SbbInputModule } from '@sbb-esta/angular/input';
+import { SbbPaginationModule } from '@sbb-esta/angular/pagination';
 import { SbbRadioButtonModule } from '@sbb-esta/angular/radio-button';
+import { SbbSelectModule } from '@sbb-esta/angular/select';
 import { SbbTableModule } from '@sbb-esta/angular/table';
 
+import { FilterSortPaginatorTableExample } from './filter-sort-paginator-table/filter-sort-paginator-table-example';
+import { GroupedColumnsTableExample } from './grouped-columns-table/grouped-columns-table-example';
+import { GroupedRowsAndColumnsTableExample } from './grouped-rows-and-columns-table/grouped-rows-and-columns-table-example';
+import { PaginatorTableExample } from './paginator-table/paginator-table-example';
+import { SelectableTableExample } from './selectable-table/selectable-table-example';
+import { SimpleTableExample } from './simple-table/simple-table-example';
+import { SortableTableExample } from './sortable-table/sortable-table-example';
 import { TableExample } from './table/table-example';
 
-export { TableExample };
+export {
+  FilterSortPaginatorTableExample,
+  GroupedColumnsTableExample,
+  GroupedRowsAndColumnsTableExample,
+  PaginatorTableExample,
+  SelectableTableExample,
+  SimpleTableExample,
+  SortableTableExample,
+  TableExample,
+};
 
-const EXAMPLES = [TableExample];
+const EXAMPLES = [
+  FilterSortPaginatorTableExample,
+  GroupedColumnsTableExample,
+  GroupedRowsAndColumnsTableExample,
+  PaginatorTableExample,
+  SelectableTableExample,
+  SimpleTableExample,
+  SortableTableExample,
+  TableExample,
+];
 
 @NgModule({
   imports: [
@@ -20,9 +48,12 @@ const EXAMPLES = [TableExample];
     ReactiveFormsModule,
     SbbButtonModule,
     SbbCheckboxModule,
-    SbbFormFieldModule,
+    SbbInputModule,
     SbbRadioButtonModule,
     SbbTableModule,
+    SbbPaginationModule,
+    SbbAutocompleteModule,
+    SbbSelectModule,
   ],
   declarations: EXAMPLES,
   exports: EXAMPLES,
