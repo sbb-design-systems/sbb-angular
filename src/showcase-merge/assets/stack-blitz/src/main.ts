0bf2366c6586a139eb7cbb9d55bd7ae1082ab553
Jeri Peier <jeremias.peier@sbb.ch>
2021-11-25 14:10:33 +0100

    fix(showcase): fix stackblitz i18n and RouterModule

--- src/showcase-merge/assets/stack-blitz/src/main.ts
+++ src/showcase-merge/assets/stack-blitz/src/main.ts
@@ -4,6 +4,12 @@ import {platformBrowserDynamic} from '@angular/platform-browser-dynamic';
 import {AppModule} from './app/app.module';
 import {environment} from './environments/environment';
 
+import '@angular/localize/init';
+import '@angular/common/locales/global/en-CH';
+import '@sbb-esta/angular/i18n';
+
+$localize.locale = 'en-CH';
+
 if (environment.production) {
   enableProdMode();
 }
