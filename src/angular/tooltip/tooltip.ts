6076f8be2f00221b13f0e0a79c713fc80adcecfa
Lukas Spirig <lukas.spirig@sbb.ch>
2021-12-08 18:34:40 +0100

    refactor: use any for setTimeout values due to DOM/NodeJS conflict

--- src/angular/tooltip/tooltip.ts
+++ src/angular/tooltip/tooltip.ts
@@ -254,7 +254,7 @@ export abstract class _SbbTooltipBase<T extends _TooltipComponentBase>
   private _document: Document;
 
   /** Timer started at the last `touchstart` event. */
-  private _touchstartTimeout: number;
+  private _touchstartTimeout: any;
 
   /** Emits when the component is destroyed. */
   private readonly _destroyed = new Subject<void>();
@@ -770,10 +770,10 @@ export abstract class _TooltipComponentBase implements OnDestroy {
   tooltipClass: string | string[] | Set<string> | { [key: string]: any };
 
   /** The timeout ID of any current timer set to show the tooltip */
-  _showTimeoutId: number | undefined;
+  _showTimeoutId: any;
 
   /** The timeout ID of any current timer set to hide the tooltip */
-  _hideTimeoutId: number | undefined;
+  _hideTimeoutId: any;
 
   /** Property watched by the animation framework to show or hide the tooltip */
   _visibility: TooltipVisibility = 'initial';
