3a3620f87cb516e9a68221df064138961b2e5ab8
Lukas Spirig <lukas.spirig@sbb.ch>
2022-02-01 09:40:03 +0100

    build: update dependencies (#1089)
    Also replaces the dev-server with the http_server from `@angular/dev-infra-private`.

--- src/angular/table/table/table.ts
+++ src/angular/table/table/table.ts
@@ -35,7 +35,7 @@ import {
   SkipSelf,
   ViewEncapsulation,
 } from '@angular/core';
-import { merge, Subject } from 'rxjs';
+import { Subject } from 'rxjs';
 import { takeUntil } from 'rxjs/operators';
 
 /**
@@ -124,11 +124,12 @@ export class SbbTable<T> extends CdkTable<T> implements OnInit, OnDestroy {
     super.ngOnInit();
     // If more than one column is sticky, the left offset is calculated at a wrong
     // time by cdk and sticky columns can get overlapped.
-    // This workaround calculates sticky styles whenever content data was changed or the viewport has changed
+    // This workaround calculates sticky styles whenever the viewport has changed
     // using a Promise.resolve() to postpone data calculation to the time the content is already placed in DOM.
     // See also https://github.com/angular/components/issues/15885.
-    this._ngZone.runOutsideAngular(() => {
-      merge(this.contentChanged, this._viewportRulerSbb.change(150))
+    this._ngZone!.runOutsideAngular(() => {
+      this._viewportRulerSbb
+        .change(150)
         .pipe(takeUntil(this._destroyed))
         .subscribe(() => {
           Promise.resolve().then(() => {
