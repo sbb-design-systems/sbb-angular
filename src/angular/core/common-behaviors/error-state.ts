8e7826f937ac3e89cb7ae3a437a2d55eacbb08de
Jeremias Peier <jeremias.peier@sbb.ch>
2022-02-21 08:33:12 +0100

    refactor(angular/core): no longer define stateChanges in `mixinErrorState`
    The error state mixin provided in `@sbb-esta/angular/core` currently
    defines the `stateChanges` class member as part of the mixin. This
    is unexpected as consumers might deal with the `stateChanges` member
    differently. e.g. in some components the `stateChanges` field is
    intended to show up in the docs, or the JSDoc description varies. e.g.
    the observable could emit whenever form-field state changes, and it
    should be updated, or it emits always when something changes (e.g.
    even a component input which is not relevant for the form-field
    control).
    
    In general, we want to avoid this member being defined in the mixin
    as the mixin is rather about the error state, and not defining a
    subject that can emit whenever "state" changes.
    
    BREAKING CHANGE: Previously the `mixinErrorState` mixin function defined
    a class member for `stateChanges`. This is no longer the case, and consumers
    need to provide the `stateChanges` class member themselves.

--- src/angular/core/common-behaviors/error-state.ts
+++ src/angular/core/common-behaviors/error-state.ts
@@ -7,18 +7,28 @@ import { AbstractConstructor, Constructor } from './constructor';
 
 /** @docs-private */
 export interface CanUpdateErrorState {
-  readonly stateChanges: Subject<void>;
-  errorState: boolean;
-  errorStateMatcher: SbbErrorStateMatcher;
+  /** Updates the error state based on the provided error state matcher. */
   updateErrorState(): void;
+  /** Whether the component is in an error state. */
+  errorState: boolean;
+  /** An object used to control the error state of the component. */
+  errorStateMatcher: SbbErrorStateMatcher;
 }
+
+type CanUpdateErrorStateCtor = Constructor<CanUpdateErrorState> &
+  AbstractConstructor<CanUpdateErrorState>;
 
 /** @docs-private */
 export interface HasErrorState {
   _parentFormGroup: FormGroupDirective;
   _parentForm: NgForm;
   _defaultErrorStateMatcher: SbbErrorStateMatcher;
+
+  // These properties are defined as per the `SbbFormFieldControl` interface. Since
+  // this mixin is commonly used with custom form-field controls, we respect the
+  // properties (also with the public name they need according to `SbbFormFieldControl`).
   ngControl: NgControl;
+  stateChanges: Subject<void>;
 }
 
 /**
@@ -27,22 +37,18 @@ export interface HasErrorState {
  */
 export function mixinErrorState<T extends AbstractConstructor<HasErrorState>>(
   base: T
-): AbstractConstructor<CanUpdateErrorState> & T;
+): CanUpdateErrorStateCtor & T;
 export function mixinErrorState<T extends Constructor<HasErrorState>>(
   base: T
-): Constructor<CanUpdateErrorState> & T {
+): CanUpdateErrorStateCtor & T {
   return class extends base {
     /** Whether the component is in an error state. */
     errorState: boolean = false;
 
-    /**
-     * Stream that emits whenever the state of the input changes such that the wrapping
-     * `SbbField` needs to run change detection.
-     */
-    readonly stateChanges = new Subject<void>();
-
+    /** An object used to control the error state of the component. */
     errorStateMatcher: SbbErrorStateMatcher;
 
+    /** Updates the error state based on the provided error state matcher. */
     updateErrorState() {
       const oldState = this.errorState;
       const parent = this._parentFormGroup || this._parentForm;
