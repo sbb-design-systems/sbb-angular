738e1feb1abb7975974fe95cab230aadaefdb50f
u237004 <mario.haertwig@sbb.ch>
2022-02-24 15:24:52 +0100

    feat(showcase): avoid 404s by only loading available example files

--- src/showcase/app/shared/component-viewer/example-viewer/example-viewer.component.ts
+++ src/showcase/app/shared/component-viewer/example-viewer/example-viewer.component.ts
@@ -1,11 +1,16 @@
 import { Component, Injector, Input, OnInit, ViewContainerRef } from '@angular/core';
 import { ActivatedRoute } from '@angular/router';
 import { ExampleData, loadExample } from '@sbb-esta/components-examples';
-import { Observable } from 'rxjs';
+import { combineLatest, Observable } from 'rxjs';
 import { map, switchMap } from 'rxjs/operators';
 
 import { HtmlLoader } from '../../html-loader.service';
 import { moduleParams } from '../../module-params';
+
+interface ExampleCode {
+  label: string;
+  code: string;
+}
 
 @Component({
   selector: 'sbb-example-viewer',
@@ -14,25 +19,54 @@ import { moduleParams } from '../../module-params';
 })
 export class ExampleViewerComponent implements OnInit {
   @Input() exampleData: ExampleData;
-  html: Observable<string>;
-  ts: Observable<string>;
-  css: Observable<string>;
+  exampleCodes: Observable<ExampleCode[]>;
   showSource = false;
-
+  private _exampleFileOrder = ['html', 'ts', 'css'];
   constructor(private _htmlLoader: HtmlLoader, private _route: ActivatedRoute) {}
 
-  ngOnInit(): void {
-    const exampleName = this.exampleData.selectorName.replace('sbb-', '').replace('-example', '');
-
-    this.html = this._createLoader(exampleName, 'html');
-    this.ts = this._createLoader(exampleName, 'ts');
-    this.css = this._createLoader(exampleName, 'css');
+  // Returns the path to the html file for a given example file.
+  private static _convertToHtmlFilePath(filePath: string): string {
+    return filePath.replace(/(.*)[.](html|ts|css)/, '$1-$2');
   }
 
-  private _createLoader(exampleName: string, type: 'html' | 'ts' | 'css') {
+  // Get the example name from the selector name
+  private static _convertToExampleName(selectorName: string): string {
+    return selectorName.replace('sbb-', '').replace('-example', '');
+  }
+
+  // Get the extension for a given file path
+  private static _getFileExtension(filePath: string): string {
+    return filePath.split('.').pop();
+  }
+
+  ngOnInit(): void {
+    this.exampleCodes = combineLatest(
+      this.exampleData.exampleFiles.map((exampleFile) =>
+        this._createLoader(
+          ExampleViewerComponent._convertToExampleName(this.exampleData.selectorName),
+          exampleFile
+        ).pipe(
+          map((code) => ({
+            label: ExampleViewerComponent._getFileExtension(exampleFile),
+            code,
+          }))
+        )
+      )
+    ).pipe(
+      map((exampleCodes: ExampleCode[]) =>
+        exampleCodes.sort(
+          (a, b) =>
+            this._exampleFileOrder.indexOf(a.label) - this._exampleFileOrder.indexOf(b.label)
+        )
+      )
+    );
+  }
+
+  private _createLoader(exampleName: string, exampleFile: string) {
+    const exampleHtmlFile = ExampleViewerComponent._convertToHtmlFilePath(exampleFile);
     return moduleParams(this._route).pipe(
       switchMap((params) =>
-        this._htmlLoader.withParams(params).fromExamples(exampleName, type).load()
+        this._htmlLoader.withParams(params).fromExamples(exampleName, exampleHtmlFile).load()
       )
     );
   }
