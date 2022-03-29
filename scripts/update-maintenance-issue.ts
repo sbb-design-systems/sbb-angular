fa600788e0be17905cbcc065e6d330e689234632
u237004 <mario.haertwig@sbb.ch>
2022-03-29 12:30:36 +0200

    ci: implement maintenance workflow
    This pull request uses GitHub actions to
    - check for `@breaking-change` annotations
    - add the "target: maintenance" label if the pr is not breaking
    - cherry-picks non-breaking changes into the maintenance branch
    - collects failed cherry-picks in an issue

---
+++ scripts/update-maintenance-issue.ts
@@ -0,0 +1,61 @@
+import { Octokit } from 'octokit';
+
+const githubToken = process.env['GITHUB_TOKEN'];
+const pullRequestNumber = parseInt(process.env['PR_NUMBER']!, 10);
+
+const repoConfig = {
+  owner: 'sbb-design-systems',
+  repo: 'sbb-angular',
+};
+
+const issuePath = {
+  ...repoConfig,
+  issue_number: 1346,
+};
+
+const prPath = {
+  ...repoConfig,
+  pull_number: pullRequestNumber,
+};
+
+export class MaintenanceIssueUpdater {
+  constructor(private _octokit: Octokit, private _now: Date) {}
+
+  async run() {
+    const issue = await this._octokit.rest.issues.get(issuePath);
+    const pr = await this._octokit.rest.pulls.get(prPath);
+
+    if (!issue.data.body) {
+      throw new Error('Could not load issue body');
+    }
+
+    if (!pr.data.title || !pr.data.html_url) {
+      throw new Error('Could not load pull request');
+    }
+
+    const hint = '**Unable to merge the following pull requests into the maintenance branch**';
+    const dateInfo = `${this._now.toISOString()}`;
+    let openTasks = this._extractOpenTasks(issue.data.body);
+    openTasks.push(`- [ ] [${pr.data.title}](${pr.data.html_url})`);
+    openTasks = [...new Set(openTasks)];
+
+    return this._octokit.rest.issues.update({
+      ...issuePath,
+      body: `${hint}\r\n${openTasks.join('\r\n')}\r\n${dateInfo}`,
+    });
+  }
+
+  private _extractOpenTasks(issueBody: string = ''): string[] {
+    return issueBody.split('\r\n').filter((line) => line.startsWith('- [ ]'));
+  }
+}
+
+if (module === require.main) {
+  const maintenanceIssueUpdater = new MaintenanceIssueUpdater(
+    new Octokit({
+      auth: githubToken,
+    }),
+    new Date()
+  );
+  maintenanceIssueUpdater.run();
+}
