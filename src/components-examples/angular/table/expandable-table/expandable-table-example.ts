7ff81966047bca395d753265a46f2ba55608aa4a
u237004 <mario.haertwig@sbb.ch>
2022-04-04 10:28:41 +0200

    chore: review

--- src/components-examples/angular/table/expandable-table/expandable-table-example.ts
+++ src/components-examples/angular/table/expandable-table/expandable-table-example.ts
@@ -21,37 +21,36 @@ export class ExpandableTableExample {
 const TABLE_EXAMPLE_DATA = [
   {
     id: 1,
-    connection: 'ICE Richtung Berlin Ostbahnhof',
+    connection: 'ICE Direction Berlin Ostbahnhof',
     time: '11:04 - 11:30',
     duration: '26 min',
   },
   { id: 1, detailRow: true, changes: 0, details: 'Bern, Gleis 6; Olten, Gleis 7' },
   {
     id: 2,
-    connection: 'IR 17 Richtung Olten',
+    connection: 'IR 17 Direction Olten',
     time: '11:07 - 11:54',
     duration: '47 min',
   },
   { id: 2, detailRow: true, details: 'Bern, Gleis 10; Olten, Gleis 10' },
   {
     id: 3,
-    connection: 'IR 16 Richtung Zürich HB',
+    connection: 'IR 16 Direction Zürich HB',
     time: '11:33 - 12:00',
     duration: '27 min',
   },
   { id: 3, detailRow: true, changes: 0, details: 'Bern, Gleis 12; Olten, Gleis 4' },
   {
     id: 4,
-    connection: 'IC 61 Richtung Basel SBB',
+    connection: 'IC 61 Direction Basel SBB',
     time: '11:36 - 12:03',
     duration: '27 min',
   },
   { id: 4, detailRow: true, changes: 0, details: 'Bern, Gleis 4; Olten, Gleis 7' },
   {
     id: 5,
-    connection: 'IR 35 Richtung Chur',
+    connection: 'IR 35 Direction Chur',
     time: '11:38 - 12:24',
     duration: '46 min',
   },
-  { id: 5, detailRow: true, changes: 0, details: 'Bern, Gleis 7; Olten, Gleis 4' },
 ];
