fe3bcbb58bb08d2a6f142f1ea6962b02f8d7ec6f
u237004 <mario.haertwig@sbb.ch>
2022-04-04 10:45:48 +0200

    chore: simplify expandable table example

--- src/components-examples/angular/table/expandable-table/expandable-table-example.ts
+++ src/components-examples/angular/table/expandable-table/expandable-table-example.ts
@@ -14,8 +14,7 @@ export class ExpandableTableExample {
   dataSource = TABLE_EXAMPLE_DATA;
   selectedId: number;
 
-  isExpansionDetailRow = (i: number, row: Object) => row.hasOwnProperty('detailRow');
-  isNotExpansionDetailRow = (i: number, row: Object) => !row.hasOwnProperty('detailRow');
+  isExpansionDetailRow = (i: number, row: Object) => row.hasOwnProperty('details');
 }
 
 const TABLE_EXAMPLE_DATA = [
@@ -24,29 +23,32 @@ const TABLE_EXAMPLE_DATA = [
     connection: 'ICE Direction Berlin Ostbahnhof',
     time: '11:04 - 11:30',
     duration: '26 min',
+    details: 'Bern, Gleis 6; Olten, Gleis 7',
   },
-  { id: 1, detailRow: true, changes: 0, details: 'Bern, Gleis 6; Olten, Gleis 7' },
+
   {
     id: 2,
     connection: 'IR 17 Direction Olten',
     time: '11:07 - 11:54',
     duration: '47 min',
+    details: 'Bern, Gleis 10; Olten, Gleis 10',
   },
-  { id: 2, detailRow: true, details: 'Bern, Gleis 10; Olten, Gleis 10' },
+
   {
     id: 3,
     connection: 'IR 16 Direction Zürich HB',
     time: '11:33 - 12:00',
     duration: '27 min',
+    details: 'Bern, Gleis 12; Olten, Gleis 4',
   },
-  { id: 3, detailRow: true, changes: 0, details: 'Bern, Gleis 12; Olten, Gleis 4' },
   {
     id: 4,
     connection: 'IC 61 Direction Basel SBB',
     time: '11:36 - 12:03',
     duration: '27 min',
+    details: 'Bern, Gleis 4; Olten, Gleis 7',
   },
-  { id: 4, detailRow: true, changes: 0, details: 'Bern, Gleis 4; Olten, Gleis 7' },
+
   {
     id: 5,
     connection: 'IR 35 Direction Chur',
