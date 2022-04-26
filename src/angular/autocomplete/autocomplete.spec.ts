ae9e082d04122440a132d74362a2a805b931aba1
Jeri Peier <jeremias.peier@sbb.ch>
2022-04-26 09:52:29 +0200

    fix(angular/autocomplete): outside click in Angular zone (#1409)
    Fixes a bug in `autocomplete` when outside click doesn't trigger `changeDetection`.

--- src/angular/autocomplete/autocomplete.spec.ts
+++ src/angular/autocomplete/autocomplete.spec.ts
@@ -3781,6 +3781,31 @@ describe('SbbAutocomplete', () => {
     expect(fixture.componentInstance.trigger.panelOpen).toBe(true);
   });
 
+  it('should emit from `autocomplete.closed` after click outside inside the NgZone', fakeAsync(() => {
+    const inZoneSpy = jasmine.createSpy('in zone spy');
+
+    const fixture = createComponent(SimpleAutocomplete, [
+      { provide: NgZone, useFactory: () => new NgZone({ enableLongStackTrace: false }) },
+    ]);
+    const ngZone = TestBed.inject(NgZone);
+    fixture.detectChanges();
+
+    fixture.componentInstance.trigger.openPanel();
+    fixture.detectChanges();
+    flush();
+
+    const subscription = fixture.componentInstance.trigger.autocomplete.closed.subscribe(() =>
+      inZoneSpy(NgZone.isInAngularZone())
+    );
+    ngZone.onStable.emit(null);
+
+    dispatchFakeEvent(document, 'click');
+
+    expect(inZoneSpy).toHaveBeenCalledWith(true);
+
+    subscription.unsubscribe();
+  }));
+
   describe('highlighting', () => {
     let fixture: ComponentFixture<AutocompleteLocaleNormalizer>;
     let input: HTMLInputElement;
