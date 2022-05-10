b425c33e775262c9f52f0609a59cbc983f218540
u237004 <mario.haertwig@sbb.ch>
2022-05-10 14:18:45 +0200

    fix: add migration and fix tests

--- src/angular/schematics/ng-update/data/class-names.ts
+++ src/angular/schematics/ng-update/data/class-names.ts
@@ -1,3 +1,23 @@
-import { ClassNameUpgradeData, VersionChanges } from '@angular/cdk/schematics';
+import { ClassNameUpgradeData, TargetVersion, VersionChanges } from '@angular/cdk/schematics';
 
-export const classNames: VersionChanges<ClassNameUpgradeData> = {};
+export const classNames: VersionChanges<ClassNameUpgradeData> = {
+  [TargetVersion.V14 as TargetVersion]: [
+    {
+      pr: '',
+      changes: [
+        {
+          replace: 'SbbLoading',
+          replaceWith: 'SbbLoadingIndicator',
+        },
+        {
+          replace: 'SbbLoadingModule',
+          replaceWith: 'SbbLoadingIndicatorModule',
+        },
+        {
+          replace: 'SbbLoadingMode',
+          replaceWith: 'SbbLoadingIndicatorMode',
+        },
+      ],
+    },
+  ],
+};
--- src/angular/schematics/ng-update/data/element-selectors.ts
+++ src/angular/schematics/ng-update/data/element-selectors.ts
@@ -1,3 +1,15 @@
-import { ElementSelectorUpgradeData, VersionChanges } from '@angular/cdk/schematics';
+import { ElementSelectorUpgradeData, TargetVersion, VersionChanges } from '@angular/cdk/schematics';
 
-export const elementSelectors: VersionChanges<ElementSelectorUpgradeData> = {};
+export const elementSelectors: VersionChanges<ElementSelectorUpgradeData> = {
+  [TargetVersion.V14 as TargetVersion]: [
+    {
+      pr: '',
+      changes: [
+        {
+          replace: 'sbb-loading',
+          replaceWith: 'sbb-loading-indicator',
+        },
+      ],
+    },
+  ],
+};
