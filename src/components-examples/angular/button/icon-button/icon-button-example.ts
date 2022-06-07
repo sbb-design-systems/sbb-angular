c3107bbeb19477213614df38a00a1288e3540b84
u237004 <mario.haertwig@sbb.ch>
2022-06-07 16:09:52 +0200

    chore: add example for icon buttons

---
+++ src/components-examples/angular/button/icon-button/icon-button-example.ts
@@ -0,0 +1,13 @@
+import { Component } from '@angular/core';
+
+/**
+ * @title Icon Button Example
+ * @order 50
+ */
+@Component({
+  selector: 'sbb-icon-button-example',
+  templateUrl: 'icon-button-example.html',
+})
+export class IconButtonExample {
+  disabled = false;
+}
--- src/components-examples/angular/button/index.ts
+++ src/components-examples/angular/button/index.ts
@@ -7,16 +7,29 @@ import { SbbIconModule } from '@sbb-esta/angular/icon';
 
 import { ButtonOverviewExample } from './button-overview/button-overview-example';
 import { ButtonWithIconExample } from './button-with-icon/button-with-icon-example';
+import { IconButtonExample } from './icon-button/icon-button-example';
 import { LinkGroupExample } from './link-group/link-group-example';
 import { LinkExample } from './link/link-example';
 
-export { ButtonOverviewExample, ButtonWithIconExample, LinkExample, LinkGroupExample };
+export {
+  ButtonOverviewExample,
+  ButtonWithIconExample,
+  LinkExample,
+  LinkGroupExample,
+  IconButtonExample,
+};
 
-const EXAMPLES = [ButtonOverviewExample, ButtonWithIconExample, LinkExample, LinkGroupExample];
+const EXAMPLES = [
+  ButtonOverviewExample,
+  ButtonWithIconExample,
+  LinkExample,
+  LinkGroupExample,
+  IconButtonExample,
+];
 
 @NgModule({
   imports: [CommonModule, FormsModule, SbbButtonModule, SbbCheckboxModule, SbbIconModule],
   declarations: EXAMPLES,
   exports: EXAMPLES,
 })
-export class AutocompleteExamplesModule {}
+export class ButtonExamplesModule {}
