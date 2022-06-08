2423d27dff4333970bfe280525aac5d6738a6ece
Chris Dickinson <chris.dickinson@gmx.ch>
2022-06-08 08:37:54 +0200

    feat(journey-maps): make the home button configurable and refactor viewportOptions (#1483)
    BREAKING CHANGE:
    
    - `minZoomLevel`, `maxZoomLevel` and `maxBounds` have been moved from `viewportOptions` to `viewportBounds` input parameter
    - replace `SbbViewportOptions` with `SbbViewportDimensions` and rename input parameter from `viewportOptions` to `viewportDimensions`.

---
+++ src/journey-maps/angular/util/typeguard.ts
@@ -0,0 +1,19 @@
+import {
+  SbbBoundingBoxOptions,
+  SbbMapCenterOptions,
+  SbbViewportDimensions,
+} from '@sbb-esta/journey-maps/angular/journey-maps.interfaces';
+
+export function isSbbMapCenterOptions(
+  viewportDimensions?: SbbViewportDimensions
+): viewportDimensions is SbbMapCenterOptions {
+  return (
+    !!viewportDimensions && 'mapCenter' in viewportDimensions && 'zoomLevel' in viewportDimensions
+  );
+}
+
+export function isSbbBoundingBoxOptions(
+  viewportDimensions?: SbbViewportDimensions
+): viewportDimensions is SbbBoundingBoxOptions {
+  return !!viewportDimensions && 'boundingBox' in viewportDimensions;
+}
