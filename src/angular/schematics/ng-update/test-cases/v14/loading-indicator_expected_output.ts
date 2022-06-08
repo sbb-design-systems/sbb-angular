2efa5304cdfe2d461856dc7e469ebf0074b9f12c
u237004 <mario.haertwig@sbb.ch>
2022-06-08 09:49:52 +0200

    chore: testing schematic migrations

---
+++ src/angular/schematics/ng-update/test-cases/v14/loading-indicator_expected_output.ts
@@ -0,0 +1,16 @@
+import { Component, NgModule, ViewChild } from '@angular/core';
+import { SbbLoadingIndicatorModule, SbbLoadingIndicator } from '@sbb-esta/angular/loading';
+
+@Component({
+  selector: 'sbb-loading-test',
+  template: '<sbb-loading-indicator></sbb-loading-indicator>',
+})
+export class SbbLoadingTestComponent {
+  @ViewChild(SbbLoadingIndicator) loading: SbbLoadingIndicator;
+}
+
+@NgModule({
+  declarations: [SbbLoadingTestComponent],
+  imports: [SbbLoadingIndicatorModule],
+})
+export class SbbLoadingTestModule {}
