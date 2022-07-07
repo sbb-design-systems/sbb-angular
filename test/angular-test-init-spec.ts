de82bc2e1fc6e5997d2cd80a6c7d56b66dd7db46
Lukas Spirig <lukas.spirig@sbb.ch>
2022-07-07 10:58:11 +0200

    test: replace monkey patch with available config (#1562)

--- test/angular-test-init-spec.ts
+++ test/angular-test-init-spec.ts
@@ -9,38 +9,12 @@ import {
  * Common setup / initialization for all unit tests in sbb-angular.
  */
 
-TestBed.initTestEnvironment([BrowserDynamicTestingModule], platformBrowserDynamicTesting());
-patchConsoleToDetectWarningsOrErrors();
+TestBed.initTestEnvironment([BrowserDynamicTestingModule], platformBrowserDynamicTesting(), {
+  errorOnUnknownElements: true,
+  errorOnUnknownProperties: true,
+});
 
 (window as any).module = {};
 (window as any).isNode = false;
 (window as any).isBrowser = true;
 (window as any).global = window;
-
-/**
- * Monkey patch console warn and error to fail if a test makes calls to console.warn or console.error.
- * https://github.com/angular/angular/issues/36430
- */
-function patchConsoleToDetectWarningsOrErrors(): void {
-  console.warn = function (message?: any, ...optionalParams: any[]): void {
-    const params = optionalParams ? `\nParams: ${optionalParams}` : '';
-
-    // ESRI JS API Requires WebGL, which is deactivated in the capabilities in Test-Chromium on Github-CI:
-    if (params.indexOf('#validate(),WebGL is required but not supported') !== -1) {
-      return;
-    }
-
-    throw new Error(`Test contained console warning:\n${message}${params}`);
-  };
-  console.error = function (message?: any, ...optionalParams: any[]): void {
-    const params = optionalParams ? `\nParams: ${optionalParams}` : '';
-
-    // jasmine outputs deprecation warnings/errors for certain scenarios
-    // that do not yet affect our tests.
-    if (typeof message === 'string' && message.startsWith('DEPRECATION')) {
-      return;
-    }
-
-    throw new Error(`Test contained console error:\n${message}${params}`);
-  };
-}
