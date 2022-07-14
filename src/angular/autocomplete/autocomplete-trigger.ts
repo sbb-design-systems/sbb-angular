619262e39f7b4765fcc7e93a7f70f16416899200
u237004 <mario.haertwig@sbb.ch>
2022-07-14 09:29:11 +0200

    fix: typo

--- src/angular/autocomplete/autocomplete-trigger.ts
+++ src/angular/autocomplete/autocomplete-trigger.ts
@@ -611,12 +611,13 @@ export class SbbAutocompleteTrigger
               if (this.panelOpen) {
                 /**
                  * Fix to avoid scrolling to the top if a new element is added.
-                 * This happens because our overlay hasa dynamic height (different from @angular/material).
+                 * This happens because our overlay has a dynamic height (different from @angular/material).
                  * When the _resetBoundingBoxStyles() function in the FlexibleConnectedPositionStrategy
                  * is called, the element is reset to full height and scaled back afterwards.
+                 * During this the scroll position gets lost.
                  *
-                 * An alternative to this hack would be to either use a maxHeight property for our overlay
-                 * or to fix this in angular/components.
+                 * An alternative to this hack would be to either use a maxHeight property for our
+                 * overlay or to fix this in angular/components.
                  */
                 const scrollTop = this.autocomplete.panel.nativeElement.scrollTop;
                 this._overlayRef!.updatePosition();
