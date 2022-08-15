sbb-design-systems/sbb-angular

commit c6aad0bf1dbef9406bfa83e38029da0cb324a3b7

fix(angular/processflow): remove obsolete aria-expanded attribute (#1623)
The `aria-expanded` attribute is not valid for the `tabpanel` role (see e.g. [here](https://www.w3.org/TR/wai-aria-1.2/#tabpanel)). It can be simply removed because selecting the tab with `aria-selected` in combination with an `aria-controls` pointing to the `tabpanel` is already enough.

src/angular/processflow/processflow.html
@@ -35,7 +35,6 @@
     (@stepTransition.done)="_animationDone.next($event)"
     [id]="_getStepContentId(i)"
     [attr.aria-labelledby]="_getStepLabelId(i)"
-    [attr.aria-expanded]="selectedIndex === i"
   >
     <ng-container [ngTemplateOutlet]="step.content"></ng-container>
   </div>

src/angular/processflow/processflow.spec.ts
@@ -124,22 +124,6 @@ describe('SbbProcessflow', () => {
       expect(processflowEl.getAttribute('role')).toBe('tablist');
     });
 
-    it('should set aria-expanded of content correctly', () => {
-      const stepContents = fixture.debugElement.queryAll(By.css(`.sbb-processflow-content`));
-      const processflowComponent = fixture.debugElement.query(
-        By.directive(SbbProcessflow)
-      )!.componentInstance;
-      const firstStepContentEl = stepContents[0].nativeElement;
-      expect(firstStepContentEl.getAttribute('aria-expanded')).toBe('true');
-
-      processflowComponent.selectedIndex = 1;
-      fixture.detectChanges();
-
-      expect(firstStepContentEl.getAttribute('aria-expanded')).toBe('false');
-      const secondStepContentEl = stepContents[1].nativeElement;
-      expect(secondStepContentEl.getAttribute('aria-expanded')).toBe('true');
-    });
-
     it('should display the correct label', () => {
       const processflowComponent = fixture.debugElement.query(
         By.directive(SbbProcessflow)

commit aa5e9e6fb243d9ae7715c6f344d6207e69bd6466

fix(angular/processflow): fix bug that inactive steps are displayed
This bug was caused by c6aad0bf1dbef9406bfa83e38029da0cb324a3b7

Closes #1650

src/angular/processflow/processflow.html
@@ -30,6 +30,7 @@
   <div
     *ngFor="let step of steps; let i = index"
     class="sbb-processflow-content"
+    [class.sbb-processflow-content-hidden]="selectedIndex !== i"
     role="tabpanel"
     [@stepTransition]="_getAnimationDirection(i)"
     (@stepTransition.done)="_animationDone.next($event)"

src/angular/processflow/processflow.scss
@@ -52,7 +52,7 @@
 .sbb-processflow-content {
   outline: 0;
 
-  &[aria-expanded='false'] {
+  &.sbb-processflow-content-hidden {
     height: 0;
     overflow: hidden;
   }

src/angular/processflow/processflow.spec.ts
@@ -124,6 +124,23 @@ describe('SbbProcessflow', () => {
       expect(processflowEl.getAttribute('role')).toBe('tablist');
     });
 
+    it('should add the `sbb-processflow-content-hidden` class to invisible elements', () => {
+      const stepContents = fixture.debugElement.queryAll(By.css(`.sbb-processflow-content`));
+      const processflowComponent = fixture.debugElement.query(
+        By.directive(SbbProcessflow)
+      )!.componentInstance;
+      const firstStepContentEl = stepContents[0].nativeElement;
+      expect(firstStepContentEl.classList.contains('sbb-processflow-content-hidden')).toBeFalse();
+
+      processflowComponent.selectedIndex = 1;
+      fixture.detectChanges();
+
+      expect(firstStepContentEl.classList.contains('sbb-processflow-content-hidden')).toBeTrue();
+      expect(firstStepContentEl.clientHeight).toBe(0);
+      const secondStepContentEl = stepContents[1].nativeElement;
+      expect(secondStepContentEl.classList.contains('sbb-processflow-content-hidden')).toBeFalse();
+    });
+
     it('should display the correct label', () => {
       const processflowComponent = fixture.debugElement.query(
         By.directive(SbbProcessflow)