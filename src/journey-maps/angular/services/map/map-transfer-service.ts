602679a7cb5535a5227969bfd9cfcca641c72836
Chris Dickinson <chris.dickinson@gmx.ch>
2023-01-16 16:33:08 +0100

    feat(journey-maps): code review: simplify
    Refs: ROKAS-1204

--- src/journey-maps/angular/services/map/map-transfer-service.ts
+++ src/journey-maps/angular/services/map/map-transfer-service.ts
@@ -16,60 +16,25 @@ export class SbbMapTransferService {
     map: MaplibreMap,
     featureCollection: FeatureCollection = SBB_EMPTY_FEATURE_COLLECTION
   ): void {
-    if (isV1Style(map)) {
-      this._updateTransferV1(map, featureCollection);
-    } else {
-      this._updateTransferV2(map, featureCollection);
-    }
+    this._getSource(map).setData(featureCollection);
+    this._data = featureCollection;
   }
 
   // If we enter the station on another floor than '0' then the outdoor route should be displayed
   // on two floors. (Floor 0 and 'entrance' floor)
   updateOutdoorWalkFloor(map: MaplibreMap, level: number): void {
+    const features: Feature[] = this._data?.features ?? [];
+    if (needsFloorChange(features, level)) {
+      this._data.features = updateWalkFloor(features, level);
+      this._getSource(map).setData(this._data);
+    }
+  }
+
+  private _getSource(map: MaplibreMap): GeoJSONSource {
     if (isV1Style(map)) {
-      this._updateOutdoorWalkFloorV1(map, level);
+      return map.getSource(SBB_ROKAS_WALK_SOURCE) as GeoJSONSource;
     } else {
-      this._updateOutdoorWalkFloorV2(map, level);
+      return map.getSource(SBB_ROKAS_ROUTE_SOURCE) as GeoJSONSource;
     }
-  }
-
-  private _updateTransferV1(
-    map: MaplibreMap,
-    featureCollection: FeatureCollection = SBB_EMPTY_FEATURE_COLLECTION
-  ): void {
-    this._getSourceV1(map).setData(featureCollection);
-    this._data = featureCollection;
-  }
-
-  private _updateTransferV2(
-    map: MaplibreMap,
-    featureCollection: FeatureCollection = SBB_EMPTY_FEATURE_COLLECTION
-  ): void {
-    this._getSourceV2(map).setData(featureCollection);
-    this._data = featureCollection;
-  }
-
-  private _updateOutdoorWalkFloorV1(map: MaplibreMap, level: number): void {
-    const features: Feature[] = this._data?.features ?? [];
-    if (needsFloorChange(features, level)) {
-      this._data.features = updateWalkFloor(features, level);
-      this._getSourceV1(map).setData(this._data);
-    }
-  }
-
-  private _updateOutdoorWalkFloorV2(map: MaplibreMap, level: number): void {
-    const features: Feature[] = this._data?.features ?? [];
-    if (needsFloorChange(features, level)) {
-      this._data.features = updateWalkFloor(features, level);
-      this._getSourceV2(map).setData(this._data);
-    }
-  }
-
-  private _getSourceV1(map: MaplibreMap): GeoJSONSource {
-    return map.getSource(SBB_ROKAS_WALK_SOURCE) as GeoJSONSource;
-  }
-
-  private _getSourceV2(map: MaplibreMap): GeoJSONSource {
-    return map.getSource(SBB_ROKAS_ROUTE_SOURCE) as GeoJSONSource;
   }
 }
