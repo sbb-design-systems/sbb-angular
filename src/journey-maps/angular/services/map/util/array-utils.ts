65bb0305f1c07b700cfd292588049d14ed1850a2
Chris Dickinson <chris.dickinson@gmx.ch>
2023-01-18 10:54:23 +0100

    feat(journey-maps): cleanup
    Refs: ROKAS-1204

---
+++ src/journey-maps/angular/services/map/util/array-utils.ts
@@ -0,0 +1,11 @@
+export const groupBy = <T>(
+  array: T[],
+  groupKeyAccessor: (element: T) => any
+): { [key in any]: [T] } =>
+  array.reduce(
+    (res: any, curr: T) => ({
+      ...res,
+      [groupKeyAccessor(curr)]: [...(res[groupKeyAccessor(curr)] || []), curr],
+    }),
+    {}
+  );
