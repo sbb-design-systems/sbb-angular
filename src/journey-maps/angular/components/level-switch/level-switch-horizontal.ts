5823a43b7b48370011b6d2b685530022fa4cc736
Chris Dickinson <christopher.dickinson@sbb.ch>
2024-01-18 17:13:16 +0100

    feat(journey-maps): revert to 5 seconds
    Refs: ROKAS-1723

--- src/journey-maps/angular/components/level-switch/level-switch-horizontal.ts
+++ src/journey-maps/angular/components/level-switch/level-switch-horizontal.ts
@@ -41,7 +41,7 @@ export class SbbLevelSwitchHorizontal implements OnDestroy {
   @ViewChildren('sideButton') sideButtons: QueryList<ElementRef<HTMLButtonElement>>;
   showSideButtons: boolean = false;
   private countdownTimer: ReturnType<typeof setTimeout>;
-  private autoCollapseTimeout = 5000; // 5 seconds
+  private autoCollapseTimeout = 2000; // 5 seconds
 
   private _destroyed = new Subject<void>();
 
