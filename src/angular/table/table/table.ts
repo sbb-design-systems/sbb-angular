eb93ee679161c80ad94e155e76dbd6ba141396e9
u237004 <mario.haertwig@sbb.ch>
2024-01-22 14:53:05 +0100

    fix(angular/table): make it working with Angular 17.1.0
    See https://github.com/angular/components/commit/30f07054913ff1026b0ec8420cd217ccc8daa167

--- src/angular/table/table/table.ts
+++ src/angular/table/table/table.ts
@@ -57,12 +57,32 @@ export class SbbRecycleRows {}
   // element in the table. See https://developer.mozilla.org/en-US/docs/Web/HTML/Element/caption
   // We can't reuse `CDK_TABLE_TEMPLATE` because it's incompatible with local compilation mode.
   template: `
-    <ng-content select="caption"></ng-content>
-    <ng-content select="colgroup, col"></ng-content>
-    <ng-container headerRowOutlet></ng-container>
-    <ng-container rowOutlet></ng-container>
-    <ng-container noDataRowOutlet></ng-container>
-    <ng-container footerRowOutlet></ng-container>
+    <ng-content select="caption" />
+    <ng-content select="colgroup, col" />
+    <!--
+        Unprojected content throws a hydration error so we need this to capture it.
+        It gets removed on the client so it doesn't affect the layout.
+      -->
+    @if (_isServer) {
+      <ng-content />
+    }
+    @if (_isNativeHtmlTable) {
+      <thead role="rowgroup">
+        <ng-container headerRowOutlet />
+      </thead>
+      <tbody class="mdc-data-table__content" role="rowgroup">
+        <ng-container rowOutlet />
+        <ng-container noDataRowOutlet />
+      </tbody>
+      <tfoot role="rowgroup">
+        <ng-container footerRowOutlet />
+      </tfoot>
+    } @else {
+      <ng-container headerRowOutlet />
+      <ng-container rowOutlet />
+      <ng-container noDataRowOutlet />
+      <ng-container footerRowOutlet />
+    }
   `,
   host: {
     class: 'sbb-table',
