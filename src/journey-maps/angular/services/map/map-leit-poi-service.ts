43eeae6d8e93c7908ebd33d0696eb0cf04196e94
besartmemeti <83814947+besartmemeti@users.noreply.github.com>
2024-03-13 13:05:48 +0100

    feat(journey-maps): enable start level even without leit-pois (#2214)

--- src/journey-maps/angular/services/map/map-leit-poi-service.ts
+++ src/journey-maps/angular/services/map/map-leit-poi-service.ts
@@ -16,7 +16,7 @@ export class SbbMapLeitPoiService {
   private static readonly _defaultLevel = 0;
   // check the rokas map style blue-arrow: 16.5 or 15 like LevelSwitch:
   private static readonly _leitPoiMinMapZoom = 15;
-  levelSwitched: Subject<number> = new Subject<number>();
+  levelSwitched: Subject<number | undefined> = new Subject<number | undefined>();
   destroyed: Subject<void> = new Subject<void>();
 
   private _mapZoomSubscription: any;
@@ -49,31 +49,38 @@ export class SbbMapLeitPoiService {
       .map((f) => this._convertToLeitPoiFeature(f))
       .filter((lp) => !!lp) as SbbLeitPoiFeature[];
 
-    if (this._leitPoiFeatures.length) {
-      this._registerMapZoomEvent(map);
-      const isJourney = featureCollection.features.some((f) => f.properties?.legId !== undefined);
-      const routeStartLevelFeature = featureCollection.features.find(
-        (f) =>
-          !!f.properties?.step &&
-          f.properties?.routeStartLevel &&
-          // a /journey feature with `routeStartLevel` should always have a defined `legId` -> checking for `legId !== undefined` is not necessary
-          (!isJourney || f.properties?.legId === selectedLegId),
-      );
+    const isJourney = featureCollection.features.some((f) => f.properties?.legId !== undefined);
+    const routeStartLevelFeature = featureCollection.features.find(
+      (f) =>
+        !!f.properties?.step &&
+        f.properties?.stationLevels &&
+        f.properties?.routeStartLevel !== undefined &&
+        // a /journey feature with `routeStartLevel` should always have a defined `legId` -> checking for `legId !== undefined` is not necessary
+        (!isJourney || f.properties?.legId === selectedLegId),
+    );
+    if (routeStartLevelFeature) {
       const routeStartLevel = routeStartLevelFeature
         ? Number(routeStartLevelFeature.properties!.routeStartLevel)
         : SbbMapLeitPoiService._defaultLevel;
 
-      const switchIt = () => {
-        this.setCurrentLevel(map, routeStartLevel!);
-        this.levelSwitched.next(routeStartLevel!); // this triggers the actual visible level switch in the map
-      };
-
-      if (map.loaded()) {
-        switchIt();
-      } else {
-        map.once('idle', switchIt);
-      }
+      this._switchLevelMapAware(map, routeStartLevel);
+    } else {
+      this._switchLevelMapAware(map, undefined);
     }
+  }
+
+  private _switchLevelMapAware(map: MaplibreMap, routeStartLevel?: number) {
+    this._registerMapZoomEvent(map);
+    if (map.loaded()) {
+      this._switchLevel(map, routeStartLevel); // this triggers the actual visible level switch in the map
+    } else {
+      map.once('idle', () => this._switchLevel(map, routeStartLevel));
+    }
+  }
+
+  private _switchLevel(map: MaplibreMap, routeStartLevel?: number) {
+    this.setCurrentLevel(map, routeStartLevel);
+    this.levelSwitched.next(routeStartLevel);
   }
 
   setCurrentLevel(map: MaplibreMap, currentLevel: number | undefined): void {
