9f78fac3e3888552ce153238036f1c9c5eb62517
Mario Härtwig <mario.haertwig@sbb.ch>
2024-06-10 15:24:30 +0200

    feat(angular/sidebar): make sidebar collapsible (#2265)
    This allows to make the `SbbSidebar` collapsible. If collapsible, the sidebar is displayed in over mode (above the content) and can be closed by clicking the close button. The toggleSidebar() method can be used to open or collapse the sidebar.
    
    Closes #2263

--- src/components-examples/angular/sidebar/index.ts
+++ src/components-examples/angular/sidebar/index.ts
@@ -1,3 +1,4 @@
 export { MultipleSidebarsExample } from './multiple-sidebars/multiple-sidebars-example';
 export { IconSidebarExample } from './icon-sidebar/icon-sidebar-example';
 export { SidebarExample } from './sidebar/sidebar-example';
+export { CollapsibleSidebarExample } from './collapsible-sidebar/collapsible-sidebar-example';
