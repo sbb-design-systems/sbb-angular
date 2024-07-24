5542da61a075648cbbbfa426b6e83c46590e01f1
Mario Härtwig <mario.haertwig@sbb.ch>
2024-07-24 10:30:58 +0200

    fix(angular/datepicker): avoid losing focus when re-rendering the current view (#2332)

--- src/angular/datepicker/calendar/calendar.ts
+++ src/angular/datepicker/calendar/calendar.ts
@@ -325,12 +325,16 @@ export class SbbCalendar<D> implements AfterContentInit, AfterViewChecked, OnDes
         ? changes['maxDate']
         : undefined;
 
-    const change = minDateChange || maxDateChange || changes['dateFilter'];
+    const changeRequiringRerender = minDateChange || maxDateChange || changes['dateFilter'];
 
-    if (change && !change.firstChange) {
+    if (changeRequiringRerender && !changeRequiringRerender.firstChange) {
       const view = this._getCurrentViewComponent();
 
       if (view) {
+        // Schedule focus to be moved to the active date since re-rendering
+        // can blur the active cell.
+        this._moveFocusOnNextTick = true;
+
         // We need to `detectChanges` manually here, because the `minDate`, `maxDate` etc. are
         // passed down to the view via data bindings which won't be up-to-date when we call `_init`.
         this._changeDetectorRef.detectChanges();
