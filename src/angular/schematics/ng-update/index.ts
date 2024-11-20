c0ed83016c99fd2eaf7bb4c8302be92e94930772
Mario Härtwig <mario.haertwig@sbb.ch>
2024-11-20 09:56:56 +0100

    build: prepare the v19 release (#2434)

--- src/angular/schematics/ng-update/index.ts
+++ src/angular/schematics/ng-update/index.ts
@@ -4,7 +4,7 @@ import { createMigrationSchematicRule, TargetVersion } from '@angular/cdk/schema
 import { sbbAngularUpgradeData } from './upgrade-data';
 
 /** Entry point for the migration schematics with target of Angular 18 */
-export function updateToV18(): Rule {
+export function updateToV19(): Rule {
   return createMigrationSchematicRule(
     TargetVersion.V19,
     [],
