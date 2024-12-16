89e385e33cbfc05be469b820920ca06c527ee1e7
Swiss Chris <632091+swiss-chris@users.noreply.github.com>
2024-12-16 11:53:37 +0100

    fix(journey-maps): allow showing default and custom 3d-buildings ... (#2453)
    …independently
    
    Refs: ROKAS-2163

--- src/journey-maps/angular/services/map/map-extrusion-service.ts
+++ src/journey-maps/angular/services/map/map-extrusion-service.ts
@@ -8,11 +8,11 @@ import { SBB_EMPTY_FEATURE_COLLECTION } from './map-service';
 
 @Injectable({ providedIn: 'root' })
 export class SbbMapExtrusionService {
-  addExtrusions(
+  updateCustomExtrusions(
     map: MaplibreMap,
-    extrusions: FeatureCollection = SBB_EMPTY_FEATURE_COLLECTION,
+    customExtrusions: FeatureCollection = SBB_EMPTY_FEATURE_COLLECTION,
   ): void {
     const source = map.getSource(SBB_ROKAS_EXTRUSION_SOURCE) as GeoJSONSource;
-    source.setData(extrusions);
+    source.setData(customExtrusions);
   }
 }
