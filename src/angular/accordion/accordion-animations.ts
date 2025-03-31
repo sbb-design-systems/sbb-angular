244765fbe7eded84e130711eecebd4738a075481
u237004 <mario.haertwig@sbb.ch>
2025-03-31 15:33:45 +0200

    fix(angular/accordion): remove dependency on animations module

--- src/angular/accordion/accordion-animations.ts
+++ src/angular/accordion/accordion-animations.ts
@@ -1,12 +1,3 @@
-import {
-  animate,
-  AnimationTriggerMetadata,
-  state,
-  style,
-  transition,
-  trigger,
-} from '@angular/animations';
-
 /** Time and timing curve for expansion panel animations. */
 // Note: Keep this in sync with the Sass variable for the panel header animation.
 export const SBB_EXPANSION_PANEL_ANIMATION_TIMING = '225ms cubic-bezier(0.4,0.0,0.2,1)';
@@ -36,28 +27,78 @@ export const SBB_EXPANSION_PANEL_ANIMATION_TIMING = '225ms cubic-bezier(0.4,0.0,
  * @breaking-change 21.0.0
  */
 export const sbbExpansionAnimations: {
-  readonly indicatorRotate: AnimationTriggerMetadata;
-  readonly bodyExpansion: AnimationTriggerMetadata;
+  readonly indicatorRotate: any;
+  readonly bodyExpansion: any;
 } = {
+  // Represents:
+  // trigger('indicatorRotate', [
+  //   state('collapsed, void', style({ transform: 'rotate(90deg)' })),
+  //   state('expanded', style({ transform: 'rotate(-90deg)' })),
+  //   transition(
+  //     'expanded <=> collapsed, void => collapsed',
+  //     animate(SBB_EXPANSION_PANEL_ANIMATION_TIMING),
+  //   ),
+  // ]),
+
   /** Animation that rotates the indicator arrow. */
-  indicatorRotate: trigger('indicatorRotate', [
-    state('collapsed, void', style({ transform: 'rotate(90deg)' })),
-    state('expanded', style({ transform: 'rotate(-90deg)' })),
-    transition(
-      'expanded <=> collapsed, void => collapsed',
-      animate(SBB_EXPANSION_PANEL_ANIMATION_TIMING),
-    ),
-  ]),
-  /** Animation that expands and collapses the panel content. */
-  bodyExpansion: trigger('bodyExpansion', [
-    state('collapsed, void', style({ height: '0px', visibility: 'hidden' })),
-    // Clear the `visibility` while open, otherwise the content will be visible when placed in
-    // a parent that's `visibility: hidden`, because `visibility` doesn't apply to descendants
-    // that have a `visibility` of their own (see #27436).
-    state('expanded', style({ height: '*', visibility: '' })),
-    transition(
-      'expanded <=> collapsed, void => collapsed',
-      animate(SBB_EXPANSION_PANEL_ANIMATION_TIMING),
-    ),
-  ]),
+  indicatorRotate: {
+    type: 7,
+    name: 'indicatorRotate',
+    definitions: [
+      {
+        type: 0,
+        name: 'collapsed, void',
+        styles: { type: 6, styles: { transform: 'rotate(90deg)', offset: null } },
+      },
+      {
+        type: 0,
+        name: 'expanded',
+        styles: { type: 6, styles: { transform: 'rotate(-90deg)', offset: null } },
+      },
+      {
+        type: 1,
+        expr: 'expanded <=> collapsed, void => collapsed',
+        animation: { type: 4, timings: 'cubic-bezier(0.4,0.0,0.2,1)' },
+        options: null,
+      },
+    ],
+    options: {},
+  },
+
+  // Represents:
+  // trigger('bodyExpansion', [
+  //   state('collapsed, void', style({ height: '0px', visibility: 'hidden' })),
+  //   // Clear the `visibility` while open, otherwise the content will be visible when placed in
+  //   // a parent that's `visibility: hidden`, because `visibility` doesn't apply to descendants
+  //   // that have a `visibility` of their own (see #27436).
+  //   state('expanded', style({ height: '*', visibility: '' })),
+  //   transition(
+  //     'expanded <=> collapsed, void => collapsed',
+  //     animate(SBB_EXPANSION_PANEL_ANIMATION_TIMING),
+  //   ),
+  // ]),
+
+  bodyExpansion: {
+    type: 7,
+    name: 'bodyExpansion',
+    definitions: [
+      {
+        type: 0,
+        name: 'collapsed, void',
+        styles: { type: 6, styles: { height: '0px', visibility: 'hidden' }, offset: null },
+      },
+      {
+        type: 0,
+        name: 'expanded',
+        styles: { type: 6, styles: { height: '*', visibility: '' }, offset: null },
+      },
+      {
+        type: 1,
+        expr: 'expanded <=> collapsed, void => collapsed',
+        animation: { type: 4, timings: SBB_EXPANSION_PANEL_ANIMATION_TIMING },
+        options: null,
+      },
+    ],
+    options: {},
+  },
 };
