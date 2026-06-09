a04fd403b3ebce3c87bac99cd13a2cec1a3e0d43
Mario Härtwig <mario.haertwig@sbb.ch>
2026-06-09 14:15:37 +0200

    fix(angular/select): remove deprecated factory functions (#2809)
    BREAKING CHANGE:
    * `SBB_SELECT_SCROLL_STRATEGY_PROVIDER` has been removed.
    * `SBB_SELECT_SCROLL_STRATEGY_PROVIDER_FACTORY` has been removed.

--- src/angular/select/select.module.ts
+++ src/angular/select/select.module.ts
@@ -4,11 +4,10 @@ import { SbbOptionModule } from '@sbb-esta/angular/core';
 import { SbbCommonModule } from '@sbb-esta/angular/core';
 import { SbbIconModule } from '@sbb-esta/angular/icon';
 
-import { SbbSelect, SBB_SELECT_SCROLL_STRATEGY_PROVIDER } from './select';
+import { SbbSelect } from './select';
 
 @NgModule({
   imports: [OverlayModule, SbbCommonModule, SbbIconModule, SbbOptionModule, SbbSelect],
   exports: [SbbOptionModule, OverlayModule, SbbSelect],
-  providers: [SBB_SELECT_SCROLL_STRATEGY_PROVIDER],
 })
 export class SbbSelectModule {}
